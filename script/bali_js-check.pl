7035ed7a9b6a0f4c41ba17a358b6285edde225e1
rodrigolive <rodrigolive@gmail.com>
2011-11-28 19:56:29 +0100

    js-check fine tuning

--- script/bali_js-check.pl
+++ script/bali_js-check.pl
@@ -2,11 +2,13 @@ use v5.10;
 use strict;
 use Path::Class;
 
-say "Javascript Checker. Running...";
+my $dir = shift || 'root';
+say "Javascript Checker. Running for $dir...";
+die "Error: $dir does not exist\n" unless -e $dir;
 
-dir("root")->recurse(callback=>sub{
+dir($dir)->recurse(callback=>sub{
     my $f=shift;
-    return unless $f =~ /\.js$/;
+    return unless $f =~ /\.js|\.mas$/;
     my $d = $f->slurp;
 
     for my $err ( $d =~ /(\n.*\n)(,\s+[\]|\}])/gsm ) {
