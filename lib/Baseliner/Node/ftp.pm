10c7d3bf17a43ac40916041b67729f775783cd56
clarive@ic <clarive@ic>
2012-02-13 19:53:24 +0100

    node ftp fixes, with timout and rc

--- lib/Baseliner/Node/ftp.pm
+++ lib/Baseliner/Node/ftp.pm
@@ -8,7 +8,7 @@ has ftp => ( is=>'rw', isa=>'Net::FTP', required=>1, lazy=>1,
     default => sub {
         my $self = shift;
         require Net::FTP;
-        my $ftp = Net::FTP->new( $self->resource->host )
+        my $ftp = Net::FTP->new( $self->resource->host, Timeout => 5000 )
             or _fail _loc "FTP: Could not connect to host %1", $self->resource->host;
         my ( $user, $password );
         if( defined $self->resource->user && defined $self->resource->password ) { 
@@ -24,6 +24,12 @@ has ftp => ( is=>'rw', isa=>'Net::FTP', required=>1, lazy=>1,
             }
         }
         $ftp->login( $user, $password ) or _fail $ftp->message;
+        if(  length $self->resource->home ) {
+            my $rc = $ftp->cwd( $self->resource->home );
+            _fail _loc "FTP: Could not change home directory to %1: %2", $self->resource->home, $ftp->message
+               unless $rc;
+        }
+        $ftp->binary;
         $ftp;
     }
 );
@@ -39,26 +45,34 @@ sub rc { }
 
 sub put_file {
     my ($self, %p) = @_;
-    $self->ftp->cwd( $p{remote} );
-    $self->ftp->put( $p{local} );
-    $self->ftp->message;
+    my $rc;
+    if( ! -e "$p{local}" ) {
+       $self->rc( 19 );
+       _fail $self->ret( _loc "FTP: could not find local file %1", $p{local} );
+    }
+    $rc = $self->ftp->cwd( "$p{remote}" ) if defined $p{remote};
+    $self->rc( $rc );
+    $rc = $self->ftp->put( "$p{local}" );
+    $self->rc( $rc );
+    $self->_throw_on_error;
+    $self->ret( $self->ftp->message );
 }
 
 sub put_dir {
     my ($self, %p) = @_;
-    $self->ftp->put( $p{local}, $p{remote} );
+    $self->ftp->put( "$p{local}", "$p{remote}" );
     $self->ftp->message;
 }
 
 sub get_file {
     my ($self, %p) = @_;
-    $self->ftp->get( $p{remote}, $p{local} );
+    $self->ftp->get( "$p{remote}", "$p{local}" );
     $self->ftp->message;
 }
 
 sub get_dir {
     my ($self, %p) = @_;
-    $self->ftp->get( $p{remote}, $p{local} );
+    $self->ftp->get( "$p{remote}", "$p{local}" );
     $self->ftp->message;
 }
 
