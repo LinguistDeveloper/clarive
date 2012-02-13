f92222e6f09a9853323d079600973a009701e132
clarive@ic <clarive@ic>
2012-02-13 17:26:15 +0100

    balix agente

--- script/bali_balix.pl
+++ script/bali_balix.pl
@@ -3,6 +3,9 @@ use Baseliner::Utils;
 use BaselinerX::Comm::Balix;
 
 my %args = _get_options( @ARGV );
-my $b = BaselinerX::Comm::Balix->new( host=>$args{host}, port=>$args{port}, key=>$args{key} );
-$b->execute( $args{cmd} || 'ls' );
+#my $b = BaselinerX::Comm::Balix->new( host=>$args{host}, port=>$args{port}, key=>$args{key}, os=>$args{ );
+my $b = BaselinerX::Comm::Balix->new( %args );
+my ($rc, $ret ) = $b->execute( $args{cmd} || 'ls' );
 $b->close;
+say "RC=$rc";
+say $ret;
