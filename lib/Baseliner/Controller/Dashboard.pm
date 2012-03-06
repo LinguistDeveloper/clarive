c55666d526d3c06cc4686c798d668dba9088c2b8
raul <raul.hernandez@vass.es>
2012-03-06 13:38:15 +0100

    Modificaciones para el dashboard

---
+++ lib/Baseliner/Controller/Dashboard.pm
@@ -0,0 +1,70 @@
+package Baseliner::Controller::Dashboard;
+use Baseliner::Plug;
+use Baseliner::Utils;
+use Baseliner::Core::DBI;
+use Try::Tiny;
+use v5.10;
+
+BEGIN {  extends 'Catalyst::Controller' }
+
+
+sub list : Local {
+    my ($self, $c) = @_;
+    ##$c->stash->{username} = $c->username;
+    $c->forward('/dashboard/list_jobs');
+    ##$c->forward('/dashboard/list_sqa');
+    $c->forward('/dashboard/list_emails');
+	##$c->forward('/dashboard/list_issues);
+    $c->stash->{template} = '/comp/dashboard.js';
+}
+
+sub list_jobs: Private{
+    my ( $self, $c ) = @_;
+	my $username = $c->username;
+	my (@jobs, $job, @datas, $SQL);
+	
+	
+	my $db = Baseliner::Core::DBI->new( {model => 'Baseliner'} );
+	$SQL = "SELECT DISTINCT A.ID_JOB, NAME, SUBSTR(APPLICATION, -3) AS PROJECT, STATUS, BL
+				FROM BALI_JOB_ITEMS A,
+					(SELECT * FROM ( SELECT ID, NAME, STATUS, BL
+										FROM BALI_JOB
+										WHERE USERNAME = ?
+										ORDER BY MAXSTARTTIME DESC ) WHERE ROWNUM < 6) B
+				WHERE A.ID_JOB = B.ID
+				ORDER BY A.ID_JOB";
+
+	@jobs = $db->array_hash( $SQL , $username);
+	foreach $job (@jobs){
+	    push @datas, $job;
+	}	
+		
+	$c->stash->{jobs} =\@datas;
+}
+
+sub list_emails: Private{
+    my ( $self, $c ) = @_;
+	my $username = $c->username;
+	my (@emails, $email, @datas, $SQL);
+	
+	
+	my $db = Baseliner::Core::DBI->new( {model => 'Baseliner'} );
+	$SQL = "SELECT SUBJECT, SENDER, B.SENT, ID
+				FROM BALI_MESSAGE A,
+					(SELECT * FROM ( SELECT ID_MESSAGE, SENT
+										FROM BALI_MESSAGE_QUEUE
+										WHERE USERNAME = ? AND SWREADED = 0
+										ORDER BY SENT DESC ) WHERE ROWNUM < 6) B
+				WHERE A.ID = B.ID_MESSAGE";
+				
+
+	@emails = $db->array_hash( $SQL , $username);
+	foreach $email (@emails){
+	    push @datas, $email;
+	}	
+		
+	$c->stash->{emails} =\@datas;
+}
+
+
+1;
