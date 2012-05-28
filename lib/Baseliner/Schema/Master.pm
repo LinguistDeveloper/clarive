99454e7c2f48ea6040889c0d4ecf1eec1a36575b
rodrigolive <rodrigolive@gmail.com>
2012-05-28 22:03:21 +0200

    master parent child injection, not sure if this is good

--- lib/Baseliner/Schema/Master.pm
+++ lib/Baseliner/Schema/Master.pm
@@ -42,11 +42,13 @@ sub master_setup {
 
     my $rel_type = $from_name .'_'. $to_name;
 
+    my $foreign = "Baseliner::Schema::Baseliner::Result::$to_class";
+
     # especific 
     Baseliner::Schema::Baseliner::Result::BaliMasterRel->belongs_to( $from_name, $self,
         { "foreign.$from_col" => 'self.from_mid' } );
     Baseliner::Schema::Baseliner::Result::BaliMasterRel->belongs_to(
-        post => "Baseliner::Schema::Baseliner::Result::$to_class",
+        post => $foreign,
         { "foreign.$to_col" => 'self.to_mid' }
     );
     $self->has_many(
@@ -72,6 +74,20 @@ sub master_setup {
         'parents' => 'Baseliner::Schema::Baseliner::Result::BaliMasterRel',
         { 'foreign.to_mid' => 'self.mid' }
     );
+
+    # XXX foreign - not sure this is good, maybe children-parent should be a generic thing
+    eval "require $foreign";
+    unless( $@ ) {
+        $foreign->has_many(
+            'children' => 'Baseliner::Schema::Baseliner::Result::BaliMasterRel',
+            { 'foreign.from_mid' => 'self.mid' }
+        ) unless $foreign->can('children');
+        $foreign->has_many(
+            'parents' => 'Baseliner::Schema::Baseliner::Result::BaliMasterRel',
+            { 'foreign.to_mid' => 'self.mid' }
+        ) unless $foreign->can('parents');
+    }
+
 }
 
 1;
