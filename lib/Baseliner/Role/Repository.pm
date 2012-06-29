d1f24d2c063dea022b2724f2821ae06c5579479c
rodrigolive <rodrigolive@gmail.com>
2012-06-29 15:32:18 +0200

    repository role with provider_info

--- lib/Baseliner/Role/Repository.pm
+++ lib/Baseliner/Role/Repository.pm
@@ -1,4 +1,15 @@
 package Baseliner::Role::Repository;
 use Moose::Role;
 
+requires 'name';
+requires 'config_component';
+
+sub provider_info {
+    my $self = shift;
+    return +{
+        name => $self->name,
+        config_component => $self->config_component,
+    }
+}
+
 1;
