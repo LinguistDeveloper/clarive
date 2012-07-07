bfdde188eee405ec5583285425810730162e31f6
rodrigolive <rodrigolive@gmail.com>
2012-07-07 19:46:33 +0200

    fix job-continue bug3

--- lib/BaselinerX/Job/Service/Runner.pm
+++ lib/BaselinerX/Job/Service/Runner.pm
@@ -113,8 +113,10 @@ sub new_from_id {
     $p{step} ||= $p{same_exec} ? 'POST' : 'RUN';
     my $service_name = $p{service_name} || '';
     my $exec = delete $p{exec};
+    # defer for when we have a logger
+    my $current_service = delete $p{current_service}; 
+    my $step = delete $p{step}; 
     # instantiate myself
-    my $current_service = delete $p{current_service}; # defer later, when we have a logger
     my $job = $class->new( %p );
     my $row = $job->row;
     _log "Created job object for jobid=$p{jobid} exec=$exec";
@@ -131,6 +133,7 @@ sub new_from_id {
     # setup the logger
     my $log = $job->logger( BaselinerX::Job::Log->new({ jobid=>$p{jobid}, job=>$job }) );
     $job->current_service( $service_name );
+    $job->step( $step );
     #thaw job stash from table
     my $stash = $job->thaw;
     $log->info(_loc("Job revived"), data=>_dump($stash) );
