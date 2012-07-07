bf8b839829505fd29385fccb0c6977382d004147
rodrigolive <rodrigolive@gmail.com>
2012-07-07 14:29:14 +0200

    deploy comp

---
+++ root/comp/lifecycle/deploy.js
@@ -0,0 +1,33 @@
+(function(d) {
+    var node = d.node;
+    var action = d.action;
+    if( node == undefined ) node = {};
+    if( node.attributes == undefined ) node.attributes = {};
+    if( node.attributes.data == undefined ) node.attributes.data = {};
+
+    var ns = node.attributes.data.ns;
+    if( ns == undefined  ) {
+        Ext.Msg.alert( _('Error'), _('Missing ns') );
+        return;
+    }
+    var bl = node.attributes.data.bl;
+    if( bl == undefined  ) {
+        Ext.Msg.alert( _('Error'), _('Missing bl') );
+        return;
+    }
+    var name = node.attributes.text;
+    var job_type = action.job_type;
+    var state_name = node.attributes.data.state_name;
+    if( bl == 'new' ) {
+        bl = 'DESA';  // XXX
+    }
+    Baseliner.confirm( _('Are you sure you want to deploy %1 in baseline %2', String.format("<b>{0}</b>", name), String.format("<b>{0}</b>",_(state_name)) ), function() { 
+        Baseliner.ajaxEval( '/topic/newjob', { ns: ns, bl: bl, job_type: job_type }, function(res) {
+            if( res.success ) {
+                Baseliner.message( _('Job'), res.msg );
+            } else {
+                Ext.Msg.alert( _('Error creating job'), res.msg );
+            }
+        });
+    });
+})
