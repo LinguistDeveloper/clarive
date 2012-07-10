e42e197102dae3fa95be040bfe4b695aa8067a3f
rodrigolive <rodrigolive@gmail.com>
2012-07-10 21:00:51 +0200

    jobrunner

---
+++ lib/Baseliner/Core/JobRunner.pm
@@ -0,0 +1,32 @@
+=head DESCRIPTION
+
+A simple Job Runner
+
+=cut
+package Baseliner::Core::JobRunner;
+use Moose;
+use Baseliner::Utils;
+
+has stash => qw(is rw isa HashRef), default=>sub{{}};
+has logger => qw(is rw isa Baseliner::Role::Logger), default=>sub{ 
+    require Baseliner::Core::Logger::Base;
+    Baseliner::Core::Logger::Base->new
+};
+
+with 'Baseliner::Role::JobRunner';
+
+sub jobid { 0 }
+sub bl { '*' }
+sub step { 1 }
+sub status { }
+sub exec { }
+sub logger {
+    my $self = shift;
+    _log @_;
+}
+sub job_stash { 
+    my $self = shift;
+    $self->stash;
+}
+
+1;
