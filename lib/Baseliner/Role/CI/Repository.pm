1f8346029b119d7fae7ad671a41c0920208f6146
ricardo <ricardo.martinez@vass.es>
2012-07-10 19:40:00 +0200

    Update roles for git

--- lib/Baseliner/Role/CI/Repository.pm
+++ lib/Baseliner/Role/CI/Repository.pm
@@ -1,6 +1,8 @@
 package Baseliner::Role::CI::Repository;
 use Moose::Role;
 
+requires 'list_elements';
+requires 'checkout';
 requires 'update_baselines';
 
 1;
--- lib/Baseliner/Role/CI/Revision.pm
+++ lib/Baseliner/Role/CI/Revision.pm
@@ -1,10 +1,6 @@
 package Baseliner::Role::CI::Revision;
 use Moose::Role;
 
-requires 'list_elements';
-requires 'checkout';
-requires 'repository';
-
 1;
 
 
