bbee6560e8add63139ee3b24eaac7a19635db876
rodrigolive <rodrigolive@gmail.com>
2012-09-12 20:03:32 +0200

    rules dsl part II - model

---
+++ root/forms/notify_create.js
@@ -0,0 +1,11 @@
+(function(params){
+    var data = params.data || {};
+    return [
+        new Baseliner.UserAndRoleBox({ fieldLabel: _('TO'), name:'to', allowBlank: false, value: data.to }),
+        new Baseliner.UserAndRoleBox({ fieldLabel: _('CC'), name:'cc', allowBlank: true, value: data.cc }),
+        { xtype: 'textfield', fieldLabel: _('Subject'), name:'subject', anchor:'100%', allowBlank: false, value: data.subject },
+        { xtype: 'htmleditor', fieldLabel: _('Body'), name:'body', anchor:'100%', allowBlank: false, value: data.body }
+    ]
+})
+
+
