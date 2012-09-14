9800842c8b0cf2b76d9be67392629f5cb4b6e5fb
raul <raul.hernandez@vass.es>
2012-09-14 16:28:39 +0200

    nueva plantilla y esquema

---
+++ lib/Baseliner/Schema/Baseliner/Result/BaliTopicFieldsCustom.pm
@@ -0,0 +1,40 @@
+use utf8;
+package Baseliner::Schema::Baseliner::Result::BaliTopicFieldsCustom;
+
+=head1 NAME
+
+Baseliner::Schema::Baseliner::Result::BaliTopicFieldsCustom
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+__PACKAGE__->load_components("InflateColumn::DateTime");
+__PACKAGE__->table("bali_topic_fields_custom");
+
+__PACKAGE__->add_columns(
+  "topic_mid",
+  {
+    data_type => "numeric",
+    is_nullable => 0,
+  },
+  "name",
+  {
+    data_type => "varchar2",
+    size => 1024,
+    is_nullable => 0,
+  },
+  "value",
+  {
+    data_type => "varchar2",
+    size => 2048,
+    is_nullable => 0,
+  },
+);
+
+__PACKAGE__->set_primary_key("topic_mid");
+
+
+1;
