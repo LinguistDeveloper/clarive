d068bb86293bd683aa297217ecc1ef1caf037e2e
rodrigolive <rodrigolive@gmail.com>
2012-09-17 12:07:15 +0200

    pro

--- root/ci/project.js
+++ root/ci/project.js
@@ -2,8 +2,6 @@
     return [
        { xtype:'textarea', fieldLabel: _('Description'), name:'description', allowBlank: true, value: params.rec.description, height: 150 },
        Baseliner.ci_box({ name:'repositories', fieldLabel:_('Repository'), allowBlank: true,
-           role:'Repository', value: params.rec.repository, singleMode: false }),
-       Baseliner.ci_box({ name:'variables', fieldLabel:_('Variables'), allowBlank: true,
-           role:'Variable', value: params.rec.variables, singleMode: false })
+           role:'Repository', value: params.rec.repository, singleMode: false })
     ]
 })
