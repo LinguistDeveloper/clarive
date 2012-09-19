51bc6ae301c65e29d68e232a677b48aa90ae684e
rodrigolive <rodrigolive@gmail.com>
2012-09-19 04:29:12 +0200

    value ci out

--- lib/Baseliner/Controller/Root.pm
+++ lib/Baseliner/Controller/Root.pm
@@ -5,7 +5,7 @@ use Baseliner::Utils;
 
 
 register 'action.home.show_lifecycle' => { description => 'User can access the lifecycle panel' };
-register 'action.home.show_menu' => { description => 'User can access the menu } ;
+register 'action.home.show_menu' => { description => 'User can access the menu' } ;
 
 use Try::Tiny;
 
