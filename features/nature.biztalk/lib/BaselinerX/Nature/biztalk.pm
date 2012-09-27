f8a0b7a87d4a18a2b15903676dac7fe21fe6f678
vtscm <vtscm@hacsva66.bde.es>
2012-09-27 10:39:04 +0200

    Añadir filtro resto naturalezas

---
+++ features/nature.biztalk/lib/BaselinerX/Nature/biztalk.pm
@@ -0,0 +1,13 @@
+package BaselinerX::Nature::biztalk;
+use strict;
+use warnings;
+use 5.010;
+use Baseliner::Plug;
+use Baseliner::Utils;
+use utf8;
+
+register 'nature.biztalk' => {name => 'Biztalk',
+                            icon => 'biztalk',
+                            ns   => 'nature/biztalk'};
+
+1;
