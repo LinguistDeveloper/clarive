e03c7b47eae11494710288a838d7a878c39a712a
rodrigolive <rodrigolive@gmail.com>
2012-10-02 19:08:04 +0200

    bl in master

--- lib/Baseliner/Schema/Baseliner/Result/BaliBaseline.pm
+++ lib/Baseliner/Schema/Baseliner/Result/BaliBaseline.pm
@@ -45,10 +45,7 @@ __PACKAGE__->add_columns(
 );
 __PACKAGE__->set_primary_key("id");
 
+__PACKAGE__->load_components("+Baseliner::Schema::Master");
+__PACKAGE__->has_master;
 
-# Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-06-10 12:25:05
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:dnaPb9fkWsYFTXy36pmR1w
-
-
-# You can replace this text with custom content, and it will be preserved on regeneration
 1;
