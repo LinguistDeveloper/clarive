c77170263fd50e644835881e2d47675cc5e11611
rodrigolive <rodrigolive@gmail.com>
2012-10-17 11:37:58 +0200

    topic_admin slash

--- root/comp/topic/topic_admin.js
+++ root/comp/topic/topic_admin.js
@@ -1659,7 +1659,7 @@
 
 		var tree_fields = new Ext.tree.TreePanel({
 			title: _('Fields configuration'),
-			dataUrl: "topicadmin/list_tree_fields",
+			dataUrl: "/topicadmin/list_tree_fields",
 			layout: 'form',
 			colapsible: true,
 			useArrows: true,
