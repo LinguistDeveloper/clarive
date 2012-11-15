ebfb13570541946ae035e3bd94e299e2d57540df
rodrigolive <rodrigolive@gmail.com>
2012-11-15 09:11:26 +0100

    destination ci role

--- lib/Baseliner/Role/CI/Destination.pm
+++ lib/Baseliner/Role/CI/Destination.pm
@@ -2,6 +2,7 @@ package Baseliner::Role::CI::Destination;
 use Moose::Role;
 with 'Baseliner::Role::CI';
 with 'Baseliner::Role::ErrorThrower';
+with 'Baseliner::Role::CI::Infrastructure';
 
 sub icon { '/static/images/ci/destination.png' }
 
