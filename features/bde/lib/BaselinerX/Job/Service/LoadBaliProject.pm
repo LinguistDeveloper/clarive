e6936fd089c2a42e5ac9bd2b2d25edf8be985892
vtscm-desmond-p4426 <vtscm@desmond-p4426>
2012-12-20 23:14:34 +0100

    nuevo alta aplicacion 2

--- features/bde/lib/BaselinerX/Job/Service/LoadBaliProject.pm
+++ features/bde/lib/BaselinerX/Job/Service/LoadBaliProject.pm
@@ -113,7 +113,7 @@ sub run_once {
                        my $r = DB->BaliProject->search({ name=>$sa, id_parent=>$sa_id, nature=>$nat })->first;
                        my $r = DB->BaliProject->search({ name=>$cam, id_parent=>$sa_id, nature=>$nat })->first;
                        next if $r;
-                       DB->BaliProject->create({ name=>$sa, id_parent=>$sa_id, nature=>$nat });
+                       $r = DB->BaliProject->create({ name=>$sa, id_parent=>$sa_id, nature=>$nat });
                        push @created_mids, { id=>$r->id, name=>$sa };
                    } else {
                        my $r = DB->BaliProject->search({ name=>$cam, id_parent=>$sa_id, nature=>$nat })->first;
