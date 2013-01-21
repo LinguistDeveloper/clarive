132e8db7b7290743c97e5b53f103b9eccf9d6db2
vtscm en prusv063 <vtscm@prusv063>
2013-01-21 19:05:04 +0100

    llamada nuevo servicio get user

--- features/changeman/lib/BaselinerX/Changeman.pm
+++ features/changeman/lib/BaselinerX/Changeman.pm
@@ -460,6 +460,53 @@ sub xml_components {
     $xml
 }
 
+
+=head2 getUser
+
+Recover user for changeman job
+
+=cut
+sub getUser {
+    my ($self, %args) = @_;
+    my $package = $args{package};
+    my $cmd = 'll07 ' . $package;
+
+    #_log "components: $cmd";
+    if ( defined $args{jobName} ) {
+        return $self->execute_cmd( {cmd=>$cmd, sem=>'CHM.JobAction', job=>$args{jobName}, log=>$args{logger}, frequency=>3} );
+    } else {
+        return $self->execute_cmd( {cmd=>$cmd, sem=>'CHM.JobAction', frequency=>3} );
+    }
+}
+=head2 xml_getUser
+
+Wrapper for cache to return the generated xml 
+as a Hash with the following structure:
+
+   Name: "Changeman service name"
+   ReasonCode: 00
+   ReturnCode: 00
+   Xmlserv: {}
+
+=cut
+sub xml_getUser {
+    my ($self, %args) = @_;
+    my $xml_str = $self->getUser( %args );
+    _log "getUser " . _dump $xml_str;
+    require XML::Simple;
+    my $xml = try {
+        XML::Simple::XMLin( $xml_str, ForceArray => [qw(sites)] );
+    } catch {
+        my $err = shift;
+        _throw _loc_xml_chm( $err, 'getUser', $xml_str, _dump(\%args)  );
+    };
+
+
+    #_log "Components: " . _dump $xml;
+
+    $xml
+}
+
 # ( xmlerr, modulename, data_received )
 sub _loc_xml_chm {
    my ( $xmlerr, $modulename, $data_received, $sent ) = @_;
