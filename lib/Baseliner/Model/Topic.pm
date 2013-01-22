1e86fdb67f77bbb734bc9a5eadaaec493928405f
clarive-sun <clarive@sun>
2013-01-22 13:58:55 +0100

    model topic warning removed

--- lib/Baseliner/Model/Topic.pm
+++ lib/Baseliner/Model/Topic.pm
@@ -773,7 +773,7 @@ sub get_data {
         map { $custom_data{ $_->{name} } = $_->{value} ? $_->{value} : $_->{value_clob} }
             Baseliner->model('Baseliner::BaliTopicFieldsCustom')->search( { topic_mid => $topic_mid } )->hashref->all;
         
-        push @custom_fields, map { map { $_->{id_field} } _array $_->{fields}; } grep { $_->{type} eq 'form' } _array($meta);
+        push @custom_fields, map { map { $_->{id_field} } _array $_->{fields}; } grep { defined $_->{type} && $_->{type} eq 'form' } _array($meta);
                              
 
         for (@custom_fields){
