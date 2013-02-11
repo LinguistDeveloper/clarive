154fdf9294c42f87562e0f1699dbe2919ae7c5d1
vtscm en prusv063 <vtscm@prusv063>
2013-02-11 12:25:43 +0100

    ampliacion campo type en detalle job

--- features/bde/lib/Baseliner/Schema/Baseliner/Result/BaliJobDetailReport.pm
+++ features/bde/lib/Baseliner/Schema/Baseliner/Result/BaliJobDetailReport.pm
@@ -11,7 +11,7 @@ __PACKAGE__->add_columns(
   'id', {data_type => 'integer', is_nullable => 0, is_auto_increment => 1, sequence => 'bali_job_detail_report_seq'},
   'job_name', {data_type => 'varchar2', is_nullable => 0, size => 50},
   'package_name', {data_type => 'varchar2', is_nullable => 0, size => 255},
-  'type', {data_type => 'varchar2', is_nullable => 0, size => 25},
+  'type', {data_type => 'varchar2', is_nullable => 0, size => 50},
   'description', {data_type => 'varchar2', is_nullable => 0, size => 4000},
   'fecha', {data_type => "DATE", is_nullable => 1, size => 19},
   'cam', {data_type => 'char', is_nullable => 0, size => 3},
