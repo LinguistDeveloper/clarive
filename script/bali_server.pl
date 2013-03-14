f2f975ec8596fceefae057b353f65ed3edae3284
rodrigolive <rodrigolive@gmail.com>
2013-03-14 20:47:26 +0100

    BASELINER_SERVER renamed to BASELINER_ENGINE

--- script/bali_server.pl
+++ script/bali_server.pl
@@ -7,21 +7,21 @@ and Catalyst::ScriptRunner server mode.
 
 These are Plack modes:
 
-    BASELINER_SERVER=plack bali server 
-    BASELINER_SERVER=Starman bali server 
-    BASELINER_SERVER=Twiggy bali server 
+    BASELINER_SERVER_ENGINE=plack bali server 
+    BASELINER_SERVER_ENGINE=Starman bali server 
+    BASELINER_SERVER_ENGINE=Twiggy bali server 
 
 These are Catalyst modes:
 
-    BASELINER_SERVER=default bali server [ -d | -h | ... ]
-    BASELINER_SERVER=prefork bali server [ -d | -h | ... ]
+    BASELINER_SERVER_ENGINE=default bali server [ -d | -h | ... ]
+    BASELINER_SERVER_ENGINE=prefork bali server [ -d | -h | ... ]
 
 The main difference among them is that in Catalyst-mode
 arguments are interpreted by Catalyst before 
 being sent to Plack, so C<-d> is a working debug mode
 that otherwise would have to be set like this:
 
-    BASELINER_DEBUG=1 BASELINER_SERVER=Starman bali server
+    BASELINER_DEBUG=1 BASELINER_SERVER_ENGINE=Starman bali server
 
 =cut
 
@@ -35,7 +35,7 @@ use strict;
 
 BEGIN { $ENV{CATALYST_SCRIPT_GEN} = 99; }
 
-if( !$ENV{BASELINER_SERVER} || $ENV{BASELINER_SERVER} =~ /prefork|default/i ) {
+if( !$ENV{BASELINER_SERVER_ENGINE} || $ENV{BASELINER_SERVER_ENGINE} =~ /prefork|default/i ) {
     require Catalyst::ScriptRunner;
     Catalyst::ScriptRunner->run('Baseliner', 'Server');
 } else {
@@ -43,8 +43,8 @@ if( !$ENV{BASELINER_SERVER} || $ENV{BASELINER_SERVER} =~ /prefork|default/i ) {
 
 
     my $runner = Plack::Runner->new;
-    $runner->{server} = $ENV{BASELINER_SERVER}
-        unless $ENV{BASELINER_SERVER} =~ /plack/i;
+    $runner->{server} = $ENV{BASELINER_SERVER_ENGINE}
+        unless $ENV{BASELINER_SERVER_ENGINE} =~ /plack/i;
     say "Baseliner starting Plack server" 
         . ( $runner->{server} ? " ($runner->{server})" : "" );
     $runner->parse_options(@ARGV);
@@ -52,13 +52,4 @@ if( !$ENV{BASELINER_SERVER} || $ENV{BASELINER_SERVER} =~ /prefork|default/i ) {
 }
 
 1;
-
-__DATA__
-my $port              = $ENV{BASELINER_PORT} || $ENV{CATALYST_PORT} || 3000;
-my $min_servers = $ENV{BASELINER_SERVER_MIN} || 5;
-my $max_servers = $ENV{BASELINER_SERVER_MAX} || 50;
-my $max_requests = $ENV{BASELINER_SERVER_MAX_REQUESTS} || 1000;
-my $min_spare_servers = $ENV{BASELINER_SERVER_MIN_SPARE} || 3;
-my $max_spare_servers = $ENV{BASELINER_SERVER_MAX_SPARE} || 10;
-my $restart           = $ENV{BASELINER_RELOAD} || $ENV{CATALYST_RELOAD} || 0;
 
