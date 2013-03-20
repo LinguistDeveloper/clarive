4362090736a41e18dc92b8e68f6db9a561bd39a2
rodrigolive <rodrigolive@gmail.com>
2013-03-20 13:25:39 +0100

    about format changed and support for app_name

--- lib/Baseliner/Controller/About.pm
+++ lib/Baseliner/Controller/About.pm
@@ -5,7 +5,7 @@ use base 'Catalyst::Controller';
 use Baseliner::Plug;
 use Baseliner::Utils;
 
-register 'menu.admin.about' => { label => 'About...', url => '/about/show', title=>'About Baseliner', index=>999 };
+register 'menu.admin.about' => { label => 'About...', url => '/about/show', title=>'About ' . Baseliner->config->{app_name} // 'Baseliner', index=>999 };
 
 sub dehash {
     my $v = shift;
@@ -30,10 +30,14 @@ use Sys::Hostname;
 sub show : Local {
     my ( $self, $c ) = @_;
     my @about = map { { name=>$_, value=>$c->config->{About}->{$_} } } keys %{ $c->config->{About} || {} };
+    push @about, { name=>'Server Version', value=>$Baseliner::VERSION };
     push @about, { name=>'Perl Version', value=>$] };
     push @about, { name=>'Hostname', value=>hostname };
     push @about, { name=>'Process ID', value=>$$ };
     push @about, { name=>'Server Time', value=>_now };
+    push @about, { name=>'Server Exec', value=>$0 };
+    push @about, { name=>'Server Bin', value=>$FindBin::Bin };
+    push @about, { name=>'Server Parent ID', value=>$ENV{BASELINER_PARENT_PID} };
     #push @about, { name=>'Path', value=>join '<li>',split /;|:/,$ENV{PATH} };
     push @about, { name=>'OS', value=>$^O };
     #push @about, { name=>'Library Path', value=>join '<li>',split /;|:/,$ENV{LIBPATH} || '-' };
@@ -48,7 +52,16 @@ sub page : Local {
     $c->stash->{template} = '/aaa.html';
 }
 
-
-
+sub version : Local {
+    my ( $self, $c ) = @_;
+    my $p = $c->req->params;
+    $c->stash->{json} = try {
+        { success => \1, msg => 'ok', version=>$Baseliner::VERSION };
+    } catch {
+        my $err = shift;
+        { success => \0, msg => "$err", };
+    };
+    $c->forward('View::JSON');
+}
 
 1;
