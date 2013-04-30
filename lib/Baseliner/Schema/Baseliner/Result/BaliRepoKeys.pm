925a50a9451072ab4992ed96d0b5d29b16851023
rodrigolive <rodrigolive@gmail.com>
2013-04-30 17:36:53 +0200

    is_nullable dbic warnings

--- lib/Baseliner/Schema/Baseliner/Result/BaliRepoKeys.pm
+++ lib/Baseliner/Schema/Baseliner/Result/BaliRepoKeys.pm
@@ -10,8 +10,8 @@ __PACKAGE__->table("bali_repokeys");
 __PACKAGE__->add_columns(
   "ns", { data_type => "VARCHAR2", default_value => undef, is_nullable => 0, size => 1024, },
   "ts", { data_type => "DATE", default_value => \"SYSDATE", is_nullable => 1, size => 19, },
-  "bl", { data_type => "VARCHAR2", default_value => '*', is_nullable => 1, size => 255, },
-  "version", { data_type => "NUMBER", default_value => 0, is_nullable => 1, size => 38 },
+  "bl", { data_type => "VARCHAR2", default_value => '*', is_nullable => 0, size => 255, },
+  "version", { data_type => "NUMBER", default_value => 0, is_nullable => 0, size => 38 },
   "datatype", { data_type => "VARCHAR2", default_value => '', is_nullable => 1, size => 50, },
   "k", { data_type => "VARCHAR2", default_value => undef, is_nullable => 0, size => 255 },
   "v", { data_type => "CLOB", default_value => undef, is_nullable => 1 },
