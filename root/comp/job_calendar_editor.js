879d0a3d61f58a3dd18972fe6e09aae64e9bbcea
Raul Martin <raul.hernandez@vass.es>
2013-05-03 12:48:22 +0200

    incidencia global en carga store

--- root/comp/job_calendar_editor.js
+++ root/comp/job_calendar_editor.js
@@ -88,7 +88,7 @@
                                 value: '<% $cal->{description} %>'
                             } ] },
                         { layout: 'form', columnWidth: 0.5, items: [
-                            Baseliner.ci_box({ name:'ns', role:['Infrastructure','Project'] , fieldLabel:_('Namespace'), value: cal_ns , emptyText: _('Global'), force_set_value: cal_ns && cal_ns != '' ? true: false  }),
+                            Baseliner.ci_box({ name:'ns', role:['Infrastructure','Project'] , fieldLabel:_('Namespace'), value: cal_ns != 'Global' ? cal_ns : undefined , emptyText: _('Global'), force_set_value: cal_ns && cal_ns != '' ? true: false  }),
                             {  xtype: 'combo', 
                                        name: 'bl', 
                                        hiddenName: 'bl',
