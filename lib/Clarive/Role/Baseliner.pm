45608508477b2959c05c1d38c36fbf5e67e8fb30
rodrigolive <rodrigolive@gmail.com>
2013-05-28 13:21:37 +0200

    hash even on bali commands

--- lib/Clarive/Role/Baseliner.pm
+++ lib/Clarive/Role/Baseliner.pm
@@ -25,7 +25,6 @@ sub setup_baseliner {
     }
     
     # ENV
-    exists $ENV{BALI_ENV} and $ENV{BASELINER_CONFIG_LOCAL_SUFFIX}=$ENV{BALI_ENV};
     $ENV{BALI_ENV} ||= $self->env;
     $ENV{BASELINER_ENV} ||= $ENV{BALI_ENV};
     $ENV{BASELINER_CONFIG_LOCAL_SUFFIX} ||= $ENV{BASELINER_ENV};
@@ -46,7 +45,7 @@ sub bali_service {
     require Baseliner::Standalone;
     my $c = Baseliner::Standalone->new;
     Baseliner->app( $c );
-    $opts{ arg_list } = { map { $_ => () } keys %opts }; # so that we can differentiate between defaults and user-fed data
+    $opts{ arg_list } = { map { $_ => 1 } keys %opts }; # so that we can differentiate between defaults and user-fed data
     $opts{ args } = \%opts;
     my $logger = Baseliner->model('Services')->launch($service_name, %opts, data=>\%opts, c=>$c );
     exit ref $logger ? $logger->rc : $logger;
