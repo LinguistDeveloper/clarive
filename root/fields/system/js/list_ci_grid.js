606c75b9fa262cad8abb918576a0baf603056f10
vtscm-desmond-p4426 <vtscm@desmond-p4426>
2013-05-29 13:37:54 +0200

    topicadmin no readonly, list_ci_grid fieldlet fixes

--- root/fields/system/js/list_ci_grid.js
+++ root/fields/system/js/list_ci_grid.js
@@ -7,7 +7,7 @@ params:
     rel_type: topic_ci
     type: 'listbox'
     get_method: 'get_cis'    
-    set_method: 'set_topics'
+    set_method: 'set_cis'
     field_order: 100
     section: 'details'
     single_mode: 'false'    
@@ -17,48 +17,15 @@ params:
 	var meta = params.topic_meta;
 	var data = params.topic_data;
     
-    var topic_mid = params.topic_data.topic_mid;
+    var ci_meta = {};
+    if( meta.ci_role ) ci_meta['role'] = meta.ci_role;
     
-    //var ci_store = new Baseliner.store.CI({ baseParams: { class:'item', rel_type: meta.rel_type, mid: topic_mid } });
-    var ci_store = new Ext.data.SimpleStore({
-        fields: ['mid','name','versionid', 'icon', 'user' ],
-        data: [
-            [ 11, 'pase.pm', '34', '/static/images/icons/file.gif' , 'infroox'],
-            [ 12, 'monitor.jsp', '12', '/static/images/icons/file.gif', 'infroox' ]
-        ]
-    });
-    
-    var cols = [
-          { header: '', width: 30, dataIndex: 'icon', renderer: Baseliner.render_icon },
-          { width: 20, dataIndex: 'mid', header: _('ID') },
-          { header: _('Name'), width: 240, dataIndex: 'name', renderer: function(v){ return '<b>'+v+'</b>'} }
-        ];
-    if( meta.versions ) {
-        cols.push( { header: _('Version'), width: 240, dataIndex: 'versionid' } );
-        cols.push( { header: _('User'), width: 240, dataIndex: 'user' } );
-    }
-    
-    var ci_grid = new Ext.grid.GridPanel({
-        store: ci_store,
-        layout: 'form',
-        height: 220,
-        fieldLabel: _(meta.name_field),
-        hideHeaders: true,
-		disabled: meta ? meta.readonly : true,
-        viewConfig: {
-            headersDisabled: true,
-            enableRowBody: true,
-            //scrollOffset: 2,
-            forceFit: true
-        },
-        columns:  cols
-    });
-    
-    ci_store.on('load', function(){
-    });
-    
+    var value = data[ meta.id_field ];
+    console.log( value );
+    var cis = new Baseliner.CIGrid({ title:_( meta.name_field ), ci: ci_meta,
+        value: value , name: meta.id_field });
 	return [
-        ci_grid
+        cis
     ]
 })
 
