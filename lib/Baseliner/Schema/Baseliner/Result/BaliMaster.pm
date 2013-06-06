c6a16c917d63d89d58cdfa88a86a28d6f91abef3
rodrigolive <rodrigolive@gmail.com>
2013-06-06 19:07:02 +0200

    moniker on balimaster, active cleanup

--- lib/Baseliner/Schema/Baseliner/Result/BaliMaster.pm
+++ lib/Baseliner/Schema/Baseliner/Result/BaliMaster.pm
@@ -31,7 +31,6 @@ __PACKAGE__->add_columns(
     original      => { data_type => "date", default_value => \"sysdate" },
   },
   "versionid", { data_type => "varchar2", is_nullable => 0, size => 255, default_value=>'1' },
-  "active", { data_type => "char", is_nullable => 1, size => 1, default_value=>'1' },
   "username",
   { data_type => "varchar2", default_value=>'html', size => 1024, is_nullable=>1 },
   "yaml",
@@ -40,16 +39,21 @@ __PACKAGE__->add_columns(
   "ns",
   { data_type => "varchar2", default_value=>'', size => 2048, is_nullable=>1 },
   "active", { data_type => "char", size => 1, default_value=>'1' },
-  "moniker",
-  { data_type => "varchar2", is_nullable => 1, size => 255},  
+  "moniker", { data_type => "varchar2", default_value=>'', size => 255, is_nullable=>1 },
 );
 
 __PACKAGE__->set_primary_key("mid");
+
+#__PACKAGE__->add_unique_constraint( ns => [ qw/collection ns/ ] );
+#__PACKAGE__->add_unique_constraint( moniker => [ qw/moniker/ ] );
+
 
 sub sqlt_deploy_hook {
    my ($self, $sqlt_table) = @_;
    $sqlt_table->add_index(name =>'bali_master_idx_name', fields=>['name'] );
    $sqlt_table->add_index(name =>'bali_master_idx_collection', fields=>['collection'] );
+   $sqlt_table->add_index(name =>'bali_master_idx_collection', fields=>['moniker'] );
+   $sqlt_table->add_index(name =>'bali_master_idx_collection', fields=>['collection', 'ns'] );
 }
 
 1;
