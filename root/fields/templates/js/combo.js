a6c473c2b11607b40fb1ae3664fa9d7ea5beba38
rodrigolive <rodrigolive@gmail.com>
2013-06-14 15:18:05 +0200

    local combo fieldlet

---
+++ root/fields/templates/js/combo.js
@@ -0,0 +1,62 @@
+/*
+name: Combo
+params:
+    origin: 'template'
+    type: 'combo'
+    html: '/fields/templates/html/row_body.html'
+    js: '/fields/templates/js/combo.js'
+    field_order: 1
+    allowBlank: 0
+    section: 'body'
+    options: 'option1,option2,option3'
+*/
+
+(function(params){
+    var meta = params.topic_meta;
+    var data = params.topic_data;
+    
+    var value = data[ meta.bd_field ];
+    
+    var options = meta[ 'options' ];
+    
+    var opt_arr = [];
+    if( options != undefined  ) {
+        var vv = options.split(',');
+        Ext.each(vv, function(v){
+            opt_arr.push([ v ]);
+        });
+    }
+    
+    var store = new Ext.data.ArrayStore({
+        fields: [ meta.id_field ],
+        data : opt_arr
+    });  
+    var combo =  new Ext.form.ComboBox({
+        name: meta.id_field,
+        xtype: 'combo',
+        fieldLabel: _(meta.name_field),
+        store: store,
+        triggerAction: 'all',
+        valueField: meta.id_field,
+        editable: meta.editable || false,
+        displayField: meta.id_field,
+        mode: 'local',
+        anchor: meta.anchor || '100%',
+        value: value || meta['default_value'],
+        //style: { 'font-size': '16px', float: 'left' },
+        forceSelection: true,
+            //allowBlank: meta ? !!meta.allowBlank : false,
+            // readOnly: meta ? meta.readonly : true,
+            // hidden: meta ? (meta.hidden ? meta.hidden : false): true
+        selectOnFocus: true
+    });  
+    
+    combo.on('afterrender', function(){
+        combo.el.setHeight( meta.height || 30 );
+    });
+    
+    return [
+        combo
+    ]
+})
+
