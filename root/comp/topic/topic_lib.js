3f843cd1eb2256635e09a77db01acb2026437f03
rodrigolive <rodrigolive@gmail.com>
2013-06-18 13:14:03 +0200

    topicgrid a topic_lib

--- root/comp/topic/topic_lib.js
+++ root/comp/topic/topic_lib.js
@@ -664,3 +664,119 @@ Baseliner.TopicMain = Ext.extend( Ext.Panel, {
     }
 });
 
+Baseliner.TopicCombo = Ext.extend( Ext.form.ComboBox, {
+    minChars: 2,
+    name: 'topic',
+    displayField: 'name',
+    hiddenName: 'topic',
+    valueField: 'mid',
+    msgTarget: 'under',
+    forceSelection: true,
+    typeAhead: false,
+    loadingText: _('Searching...'),
+    resizable: true,
+    allowBlank: false,
+    lazyRender: false,
+    pageSize: 20,
+    triggerAction: 'all',
+    xxxitemSelector: 'div.search-item',
+    initComponent: function(){
+        self.listeners = {
+            beforequery: function(qe){
+                delete qe.combo.lastQuery;
+            }
+        };
+        self.xxtpl = new Ext.XTemplate( '<tpl for="."><div class="search-item">{name} {title}</div></tpl>');
+        self.xtpl = new Ext.XTemplate( '<tpl for="."><div class="search-item">',
+            '<span id="boot" style="width:200px"><span class="badge" ', 
+            ' style="float:left;padding:2px 8px 2px 8px;background: {color}"',
+            ' >{name}</span></span>',
+            '&nbsp;&nbsp;<b>{title}</b></div></tpl>' );
+        self.xdisplayFieldTpl = new Ext.XTemplate( '<tpl for=".">',
+            '<span id="boot"><span class="badge" style="float:left;padding:2px 8px 2px 8px;background: {color}; cursor:pointer;"',
+            ' onclick="javascript:Baseliner.show_topic({mid}, \'{name}\');">{name}</span></span>',
+            '</tpl>' );
+        Baseliner.TopicCombo.superclass.initComponent.call(this);
+    }
+});
+
+Baseliner.TopicGrid = Ext.extend( Ext.grid.GridPanel, {
+    height: 200,
+    initComponent: function(){
+        var self = this;
+        self.combo_store = new Baseliner.store.Topics({});
+        self.combo = new Baseliner.TopicCombo({
+            store: self.combo_store, 
+            width: 300,
+            height: 80,
+            singleMode: true, 
+            fieldLabel: _('Topic'),
+            name: 'topic',
+            hiddenName: 'topic', 
+            allowBlank: true
+        }); 
+        self.field = new Ext.form.Hidden({ name: self.name, value: self.value });
+        var btn_delete = new Baseliner.Grid.Buttons.Delete({
+            handler: function() {
+                var sm = self.getSelectionModel();
+                if (sm.hasSelection()) {
+                    Ext.each( sm.getSelections(), function( sel ){
+                        self.getStore().remove( sel );
+                    });
+                    btn_delete.disable();
+                    self.refresh_field();
+                } else {
+                    Baseliner.message( _('ERROR'), _('Select at least one row'));    
+                };                
+            }
+        });
+        self.tbar = [ self.field, self.combo, btn_delete ];
+        self.combo.on('select', function(combo,rec,ix) {
+            self.add_to_grid( rec.data );
+        });
+        self.store = new Ext.data.SimpleStore({
+            fields: ['mid','name','title' ],
+            data: []
+        });
+        self.viewConfig = {
+            headersDisabled: true,
+            enableRowBody: true,
+            forceFit: true
+        };
+        self.sm = new Baseliner.CheckboxSelectionModel({
+            checkOnly: true,
+            singleSelect:false
+        });
+        self.on('rowclick', function(grid, rowIndex, e) {
+            btn_delete.enable();
+        });		
+        self.columns = [
+            self.sm,
+            { header:_('ID'), dataIndex:'mid', hidden: true },
+            { header:_('Name'), dataIndex:'name' },
+            { header:_('Title'), dataIndex:'title' }
+        ];
+        Baseliner.TopicGrid.superclass.initComponent.call( this );
+    },
+    refresh_field: function(){
+        var self = this;
+        var mids = [];
+        self.store.each(function(row){
+            mids.push( row.data.mid ); 
+        });
+        self.field.setValue( mids.join(',') );
+    },
+    add_to_grid: function(rec){
+        var self = this;
+        var f = self.store.find( 'mid', rec.mid );
+        if( f != -1 ) {
+            Baseliner.warning( _('Warning'), _('Row already exists: %1', rec.name + '(' + rec.mid + ')' ) );
+            return;
+        }
+        var r = new self.store.recordType( rec );
+        self.store.add( r );
+        self.store.commitChanges();
+        self.refresh_field();
+    }
+});
+
