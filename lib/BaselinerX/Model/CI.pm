6fbdf7d7720e5eb3ebde4c84f78153ef00ba9129
Ricardo Martínez <ricardo.martinez@vass.es>
2013-06-20 11:44:02 +0200

    Actions in CI controller

---
+++ lib/BaselinerX/Model/CI.pm
@@ -0,0 +1,29 @@
+package BaselinerX::Model::CI;
+use Baseliner::Plug;
+use Baseliner::Utils;
+use Baseliner::Sugar;
+use Path::Class;
+use Try::Tiny;
+use Proc::Exists qw(pexists);
+use Array::Utils qw(:all);
+use v5.10;
+
+BEGIN { extends 'Catalyst::Model' }
+
+register 'registor.action.ci' => {
+    generator => sub {
+		my %actions_admin_cis;
+		for my $role ( Baseliner::Controller::CI->list_roles ) {
+
+			my $name = $role->{role};
+			for my $class ( packages_that_do( $role->{role} ) ) {
+		    	my ($collection) = $class =~ /::CI::(.*?)$/;
+		    	my $id_action_admin = "action.ci.admin.".$role->{name}.".".$collection;
+		    	$actions_admin_cis{$id_action_admin} = { name => $id_action_admin };
+		    	my $id_action_view = "action.ci.view.".$role->{name}.".".$collection;
+		    	$actions_admin_cis{$id_action_view} = { name => $id_action_view };
+		    }
+		}
+        return \%actions_admin_cis;    
+    }
+};
