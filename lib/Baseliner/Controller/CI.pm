7d3f179306aa08819127e3efe004f75359dcc062
rodrigolive <rodrigolive@gmail.com>
2013-06-24 19:40:43 +0200

    ci children error fixed

--- lib/Baseliner/Controller/CI.pm
+++ lib/Baseliner/Controller/CI.pm
@@ -664,7 +664,7 @@ sub update : Local {
     my $collection = delete $p->{collection};
     $action ||= delete $p->{action};
     my $class = "BaselinerX::CI::$collection";    # XXX what?? fix the class vs. collection mess
-
+    my $chi = delete $p->{children};
     try {
         if( $action eq 'add' ) {
             $mid = $class->save( name=>$name, bl=>$bl, active=>$active, moniker=>delete($p->{moniker}), data=> $p ); 
@@ -675,7 +675,7 @@ sub update : Local {
         else {
             _fail _loc("Undefined action");
         }
-        if( my $chi = $p->{children} ) {
+        if( $chi ) {
             my $cis = ref $chi eq 'ARRAY' ? $chi : [ split /,/, $chi ]; 
             DB->BaliMasterRel->search({ from_mid=>$mid })->delete;
             for my $to_mid ( _array( $cis ) ) {
