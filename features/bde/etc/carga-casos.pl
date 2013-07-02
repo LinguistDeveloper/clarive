7d2cb51998896775bd2d58cce084d6be559e1515
vtscm-desmond-p4426 <vtscm@desmond-p4426>
2013-07-02 20:21:56 +0200

    bde stuff

--- features/bde/etc/carga-casos.pl
+++ features/bde/etc/carga-casos.pl
@@ -1,18 +1,18 @@
 use Text::CSV;
 
 my @rows;
-my $csv = Text::CSV->new ({ sep_char=>';' })  # should set binary attribute.
+my $csv = Text::CSV->new ({ sep_char=>';', binary=>1 })  # should set binary attribute.
                 or die "Cannot use CSV: ".Text::CSV->error_diag ();
  
-open my $fh, "<:encoding(utf-8)", $c->path_to("features/bde/etc/ecp1.csv") or die "$!";
+open my $fh, "<:encoding(utf-8)", $c->path_to("features/bde/etc/ecp2.csv") or die "$!";
 my @cols;
 my %cp;
 use List::MoreUtils qw/zip/;
 my $cnt=1;
 my $errs = 0;
-while ( my $row = <$fh> ) {
-    $csv->parse( $row ) or do { warn "Linea $cnt: Error '" . $csv->error_diag . "' parseando: $row\n\n"; $errs++; next };
-    $row = [ $csv->fields ];
+while ( my $row = $csv->getline($fh) ) {
+    #$csv->parse( $row ) or do { warn "Linea $cnt: Error '" . $csv->error_diag . "' parseando: $row\n\n"; $errs++; next };
+    #$row = [ $csv->fields ];
     #$row->[2] =~ m/pattern/ or next; # 3rd field should match
     if( !@cols ) {
        @cols = map { Util->_name_to_id($_) } @$row;
@@ -23,13 +23,30 @@ while ( my $row = <$fh> ) {
     push @rows, $h;
     $cnt++;
 }
+close $fh;
+#die _dump( \@rows );
 #my $estatus = 9; # en curso
 my $ecat = 26; # ECP
 my $cstatus = 5; # disponible
 my $ccat = 28; # Caso de Prueba
+my $func_cat = 2;
 my $prj_mid = 58; # SCM
 my $res = { 'correcto' => 14, 'incorrecto'=>15 };  # ECP Correcto / Incorrecto
 my $estatus = 8; # no iniciado
+my $ppcat = 29;
+my $ppstatus = 12; # Finalizado
+my $now = Class::Date->now;
+
+my ($ppmsg, $ppmid, $ppstatus, $pptitle) = Baseliner->model('Topic')->update({
+            action=>'add',
+            title => "Importación $now",
+            description=>'plan importado desde el Excel',
+            category => $ppcat,
+            status_new => $ppstatus,
+            username => $c->username || 'baseliner',
+            aplicacion => $prj_mid,
+            active => 1,
+});
 
 my $i=0;
 my $cup = my $eup = my $cadd = my $eadd = 0;
@@ -70,8 +87,10 @@ for my $r ( @rows ) {
     ];
     my $title = "$r->{id} $r->{titulo}";
     # CASO 
-    my $func = DB->BaliTopic->search({ 'lower(title)'=>{ -like=>'%'.lc($$r{funcionalidades}).'%'} },{ select=>'mid' })->first;        
-    my @deps = DB->BaliTopic->search({ 'lower(title)'=>{ -like=>'%'.lc($$r{dependencias_cps}).'%'} },{ select=>'mid' })->hashref->all;
+    my $func = DB->BaliTopic->search({ 'lower(title)'=>{ -like=>'%'.lc($$r{funcionalidades}).'%'}, id_category=>$func_cat },{ select=>'mid' })->first
+    	if length $$r{funcionalidades};    
+    my @deps = DB->BaliTopic->search({ 'lower(title)'=>{ -like=>'%'.lc($$r{dependencias_cps}).'%'}, id_category=>$ccat },{ select=>'mid' })->hashref->all
+    	if length $$r{dependencias_cps};
     my $cexists = DB->BaliTopic->search({ title=>$title, id_category=>$ccat })->first;
     my ($cmsg, $cmid, $cstatus, $ctitle) = Baseliner->model('Topic')->update({
             ( $cexists 
@@ -82,13 +101,15 @@ for my $r ( @rows ) {
             description=>$r->{descripcion} // '',
             category => $ccat,
             status_new => $cstatus,
+            dependencias => [ map { $_->{mid} } @deps ],
+            ( $func ? ( funcionalidades => [ $func->mid ] ) : () ),
             username => $c->username || 'baseliner',
             pasos => _to_json([ map { delete $_->{salida_obtenida}; $_ } _array $pasos]),
             precondiciones => _to_json([ map { delete $_->{verificado}; $_ } _array $pre]),
-            proyecto => $prj_mid,
+            aplicacion => $prj_mid,
             active => 1,
         });
-    $func and DB->BaliMasterRel->find_or_create({ from_mid=>$cmid, to_mid=>$func->mid, rel_type=>'topic_topic', rel_field=>'funcionalidades' });        
+    #$func and DB->BaliMasterRel->find_or_create({ from_mid=>$cmid, to_mid=>$func->mid, rel_type=>'topic_topic', rel_field=>'funcionalidades' });        
     for my $dep ( @deps ) {
         #DB->BaliMasterRel->find_or_create({ from_mid=>$cmid, to_mid=>$dep->{mid}, rel_type=>'topic_topic', rel_field=>'dependencias' });
     }
@@ -106,15 +127,18 @@ for my $r ( @rows ) {
             category => $ecat,
             username => $c->username || 'baseliner',
             pasos => _to_json($pasos),
+            caso_de_prueba => [$cmid],
             precondiciones => _to_json($pre),
             proyecto => $prj_mid,
             active => 1,
             status_new => $res->{$rcp} // $estatus,
         });
-        
+    
+    # añadir al plan de pruebas de importacion
+    DB->BaliMasterRel->find_or_create({ from_mid=>$ppmid, to_mid=>$emid, rel_type=>'topic_topic', rel_field=>'casos_de_prueba' });    
     $eexists ? $eup++ : $eadd++;
     $cexists ? $cup++ : $cadd++;
-    #last if ++$i > 1;
+    last if ++$i > 10;
 }
 say "Casos Nuevos: $cadd, Casos Act.: $cup, Ejecuciones Nuevas: $eadd, Ejecuciones Actualizadas: $eup";
 say "Total: @{[$errs+$cnt-1]}, Ok: @{[$cnt-1]}, Errores: $errs";
