558a31e44e886c49998fb6327e9556fc50132cf9
vtscm-desmond-p4426 <vtscm@desmond-p4426>
2013-07-18 20:37:42 +0200

    engine net added to list

--- root/ci/engine.js
+++ root/ci/engine.js
@@ -3,6 +3,7 @@
     return [
         new Baseliner.ComboSingle({ fieldLabel: _('Engine Package'), name:'engine_package', data: [
             'Baseliner::Parser::Engine::C',
+            'Baseliner::Parser::Engine::NET',
             'Baseliner::Parser::Engine::JSP',
             'Baseliner::Parser::Engine::SQL',
         ] 
