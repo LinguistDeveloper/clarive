4a9d07523b8a5674efe6b071789e12fc54cb7541
Ricardo Martínez <ricardo.martinez@vass.es>
2013-08-20 08:48:24 +0200

    Fixed _debug

--- lib/Baseliner/Utils.pm
+++ lib/Baseliner/Utils.pm
@@ -352,7 +352,8 @@ sub _error {
 
 #TODO check that global DEBUG flag is active
 sub _debug {
-    my $cal = $_[0] < 0 ? shift : 0;
+    use Scalar::Util qw(looks_like_number);
+    my $cal = looks_like_number($_[0])? ( $_[0] < 0 ? shift : 0) : 0;
     my ($cl,$fi,$li) = caller( -$cal );
     return unless Baseliner->debug;
     _log_me( 'debug', $cl,$fi,$li,@_);
