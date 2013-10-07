c5176b5b4a94397528f78d7d0433a51b9ef28604
Ricardo Martínez <ricardo.martinez@vass.es>
2013-10-07 19:18:10 +0200

    Show class in ci lists

--- root/fields/system/js/list_ci.js
+++ root/fields/system/js/list_ci.js
@@ -14,6 +14,7 @@ params:
     ci_role: 'Server'
     ci_class: ''
     rel_type: topic_ci
+    show_class: false
 ---
 */
 (function(params){
@@ -23,7 +24,7 @@ params:
     var ci = {};
     if( meta.ci_role ) ci['role'] = meta.ci_role;
     else if( meta.ci_class ) ci['class'] = meta.ci_class;
-	
+	 meta.show_class = false;
 	return [
        Baseliner.ci_box(Ext.apply({
            fieldLabel: _(meta.name_field),
@@ -32,7 +33,8 @@ params:
            force_set_value: true,
            value: data[meta.id_field],
            disabled: meta.readonly!=undefined ? meta.readonly : false,
-           allowBlank: meta.allowBlank==undefined ? true : ( meta.allowBlank == 'false' || !meta.allowBlank ? false : true )
+           allowBlank: meta.allowBlank==undefined ? true : ( meta.allowBlank == 'false' || !meta.allowBlank ? false : true ),
+           showClass: meta.show_class==undefined ? false : meta.show_class
        }, ci) )
     ]
 })
