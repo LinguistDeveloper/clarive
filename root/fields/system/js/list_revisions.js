f7189d71039cb46947228002e14c8c7d0952a90b
Raul Martin <raul.hernandez@vass.es>
2013-10-08 14:22:42 +0200

    modificacion bloqueo revision delete

--- root/fields/system/js/list_revisions.js
+++ root/fields/system/js/list_revisions.js
@@ -49,7 +49,7 @@ params:
         ],
         listeners: {
             'afterrender':function(){
-                var read_only = meta && meta.readonly ? meta.readonly : false;
+                var read_only = meta && meta.readonly ? meta.readonly : true;
                 if(read_only){
                     var mask = this.el.mask();
                     mask.setStyle('opacity', 0);
@@ -96,7 +96,7 @@ params:
         //if( c.value != undefined ) {
             // TODO no loader from mids yet 
         //}
-        var read_only = meta && meta.readonly ? meta.readonly : false;
+        var read_only = meta && meta.readonly ? meta.readonly : true;
         if( !read_only ){
             var el = this.el.dom; 
             var revision_box_dt = new Baseliner.DropTarget(el, {
