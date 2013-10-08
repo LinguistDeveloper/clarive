7679b6bca0e9468d1f6ab14b1d86891d7e939cf0
rodrigolive <rodrigolive@gmail.com>
2013-10-08 14:08:36 +0200

    debug query KV in DEBUG>1 no_record on queries

--- lib/Baseliner/Role/CI.pm
+++ lib/Baseliner/Role/CI.pm
@@ -431,6 +431,7 @@ sub load_from_query {
 
 sub query {
     my $self = shift;
+    local $Baseliner::CI::_no_record = 1;
     my @recs = map { Baseliner::Role::CI->_build_ci_instance_from_rec( $_ ) }  Baseliner::Role::CI->load_from_query( @_ );
     return @recs;
 }
