f14a6b6a6809961a10f8c4966755d18395ba2930
Ricardo Martínez <ricardo.martinez@vass.es>
2013-10-13 01:58:46 +0200

    Variable color in separator

--- root/fields/templates/js/separator.js
+++ root/fields/templates/js/separator.js
@@ -5,19 +5,21 @@ params:
     type: 'separator'
     js: '/fields/templates/js/separator.js'
     field_order: 1
+    color: '#99CCFF'
 */
 
 (function(params){
     var meta = params.topic_meta;
     var data = params.topic_data;
+    var color = meta.color ? meta.color: '#99CCFF';
     
     var separator = new Ext.Component({
         fieldLabel: _(meta.name_field),
         name: meta.id_field,
         labelSeparator: '',
-        labelStyle: 'color:#99CCFF;font-weight:bold',
+        labelStyle: 'color:'+ color + ';font-weight:bold',
         hidden: meta ? (meta.hidden ? meta.hidden : false): true,
-        autoEl: {html:'<hr style="background-color:#99CCFF;height:2px;border:none;>"'}
+        autoEl: {html:'<hr style="background-color:' + color + ';height:2px;border:none;>"'}
 
     });
     
