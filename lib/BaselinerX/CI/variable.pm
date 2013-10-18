f935caae1700efbbc40b200ab2636edd1e371ea1
rodrigolive <rodrigolive@gmail.com>
2013-10-18 12:25:50 +0200

    variable var_combo_options

--- lib/BaselinerX/CI/variable.pm
+++ lib/BaselinerX/CI/variable.pm
@@ -2,13 +2,13 @@ package BaselinerX::CI::variable;
 use Baseliner::Moose;
 use Baseliner::Utils;
 
-has var_type         => qw(is rw isa Str);
-has var_ci_class     => qw(is rw isa Maybe[Str]);
-has var_ci_role      => qw(is rw isa Maybe[Str]);
-has var_ci_mandatory => qw(is rw isa BoolCheckbox coerce 1);
-has var_ci_multiple  => qw(is rw isa BoolCheckbox coerce 1);
-has var_opts         => qw(is rw isa Any);
-has var_default      => qw(is rw isa Any);
+has var_type             => qw(is rw isa Str);
+has var_ci_class         => qw(is rw isa Maybe[Str]);
+has var_ci_role          => qw(is rw isa Maybe[Str]);
+has var_ci_mandatory     => qw(is rw isa BoolCheckbox coerce 1);
+has var_ci_multiple      => qw(is rw isa BoolCheckbox coerce 1);
+has var_combo_options    => qw(is rw isa ArrayRef), default=>sub{ [] };
+has var_default          => qw(is rw isa Any);
 
 with 'Baseliner::Role::CI::Variable';
 
