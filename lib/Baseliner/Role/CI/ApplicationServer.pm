fb092d608a40c31eb1d032685f4fff77defa1b73
rodrigolive <rodrigolive@gmail.com>
2013-10-19 12:43:38 +0200

    application server basic script attributes

--- lib/Baseliner/Role/CI/ApplicationServer.pm
+++ lib/Baseliner/Role/CI/ApplicationServer.pm
@@ -1,7 +1,13 @@
 package Baseliner::Role::CI::ApplicationServer;
 use Moose::Role;
+with 'Baseliner::Role::CI';
 
 sub icon { '/static/images/ci/appserver.png' }
+
+# has_ci 'start_script';
+# has_ci 'stop_script';
+# has_ci 'backup_script';
+# has_ci 'update_script';
 
 1;
 
