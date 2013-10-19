563cae1036926364e91ea4f2a031111f4cbf6e41
rodrigolive <rodrigolive@gmail.com>
2013-10-19 12:12:01 +0200

    removed all fake appservers from core

--- lib/BaselinerX/CI/iis.pm
+++
@@ -1,9 +0,0 @@
-package BaselinerX::CI::iis;
-use Baseliner::Moose;
-with 'Baseliner::Role::CI::ApplicationServer';
-
-sub error {}
-sub rc {}
-
-1;
-
--- lib/BaselinerX/CI/websphere.pm
+++
@@ -1,9 +0,0 @@
-package BaselinerX::CI::websphere;
-use Baseliner::Moose;
-with 'Baseliner::Role::CI::ApplicationServer';
-
-sub error {}
-sub rc {}
-
-1;
-
