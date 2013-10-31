c872184c43a87a67b0c0a0a9397125c65afc039c
rodrigolive <rodrigolive@gmail.com>
2013-10-31 14:12:40 +0100

    with_vars in all task forms

--- root/forms/nature_block.js
+++ root/forms/nature_block.js
@@ -1,7 +1,7 @@
 (function(params){
     var data = params.data || {};
     return [
-        Baseliner.ci_box({ name: 'nature', role:'Nature', fieldLabel:_('Nature'), value: params.data.nature, force_set_value: true }) 
+        Baseliner.ci_box({ name: 'nature', role:'Nature', with_vars: 1, fieldLabel:_('Nature'), value: params.data.nature, force_set_value: true }) 
     ]
 })
 
