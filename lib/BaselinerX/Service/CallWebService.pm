b338369392d30685862b4bd8b0ecb560e4ec52a1
rodrigolive <rodrigolive@gmail.com>
2013-11-05 12:03:09 +0100

    web_request timeout, headers and encoding call web service deprecated

--- lib/BaselinerX/Service/CallWebService.pm
+++ lib/BaselinerX/Service/CallWebService.pm
@@ -18,59 +18,41 @@ sub web_request {
 
     require LWP::UserAgent;
     require HTTP::Request;
+    require Encode;
     
     my $method = $config->{method} // 'GET';
     my $url    = $config->{url};
     my $args   = $config->{args};
+    my $headers = $config->{headers} || {};
+    my $timeout = $config->{timeout};
+    my $encoding = $config->{encoding} || 'utf-8';
+
+    if( $encoding ne 'utf-8' ) {
+        Encode::from_to($url, 'utf-8', $encoding ) if $url;
+        Encode::from_to($args, 'utf-8', $encoding ) if ref $args;
+    }
 
     my $uri = URI->new( $url );
     $uri->query_form( $args );
     my $request = HTTP::Request->new( $method => $uri );
     $request->authorization_basic($config->{username}, $config->{password}) if $config->{username};
     my $ua = LWP::UserAgent->new();
+    $ua->timeout( $timeout ) if $timeout;
+    for my $k ( keys %$headers ) {
+        $ua->default_header( $k => $headers->{$k} );
+    }
     $ua->env_proxy;
 
     my $response = $ua->request( $request );
 
     _fail sprintf qq/HTTP request failed: %s\nUrl: %s\nArgs: %s/, $response->status_line, $url, _to_json($args)
         unless $response->is_success;
+    if( $encoding ne 'utf-8' ) {
+        Encode::from_to($response, $encoding, 'utf-8' ) if $response;
+    }
     my $content = $response->content;
     return { response=>$response, content=>$content };
 } 
 
-
-# deprecated?
-register 'service.call_webservice' => {
-    name => 'Calls a webservice',
-    icon => '/static/images/icons/webservice.png',
-    handler => \&_call_ws,
-    data => { url => '', username => '', password => '', args => { id => '', format => ''} }
-};
-
-sub _call_ws {
-    my ( $self, $c, $data ) = @_;
-
-    _error $data;
-
-    my $url  = $data->{url};
-
-    my $args = $data->{args};
-
-    my $uri = URI->new( $url );
-    $uri->query_form( $args );
-    my $request = HTTP::Request->new( GET => $uri );
-    $request->authorization_basic($data->{username}, $data->{password}) if $data->{username};
-    my $ua = LWP::UserAgent->new();
-    $ua->env_proxy;
-
-    my $response = $ua->request( $request );
-
-    die sprintf qq/HTTP request failed: %s\nUrl: %s\nArgs: %s/, $response->status_line, $url, _dump $args
-        unless $response->is_success;
-
-    my $content = $response->content;
-
-    return { data => $content };
-} ## end sub _call_ws
 
 1;
