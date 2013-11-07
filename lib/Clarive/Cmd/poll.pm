b114cee76e7e7fa7d4bf717781171860da6f94a2
rodrigolive <rodrigolive@gmail.com>
2013-11-07 21:09:37 +0100

    cla poll pid_filter

--- lib/Clarive/Cmd/poll.pm
+++ lib/Clarive/Cmd/poll.pm
@@ -22,6 +22,7 @@ with 'Clarive::Role::TempDir';
 has url_web       => qw(is rw isa Any);
 has url_nginx     => qw(is rw isa Any);
 has api_key       => qw(is rw isa Any);
+has pid_filter    => qw(is rw isa Any);
 has mongo         => qw(is rw isa Any default 1);
 has redis         => qw(is rw isa Any default 1);
 has timeout_web   => qw(is rw isa Num default 5);
@@ -44,6 +45,7 @@ Options:
   --redis                 1=try redis connection, 0=ignore redis status
   --timeout_web           seconds to wait for clarive/nginx web response, 0=no timeout
   --error_rc              return code for fatal errors
+  --pid_filter            regular expression to filter in pid files
 
 =cut
 
@@ -69,7 +71,11 @@ sub run {
 
     my $rc = 0;
 
+    my $pid_filter = $self->pid_filter;
+    $pid_filter = qr/$pid_filter/i if $pid_filter;
+    
     for my $pidfile ( glob(file($self->pid_dir,'*.pid')), glob(file($self->app->base,'data','mongo','*.lock')) ) { 
+        next if $pid_filter && $pidfile !~ $pid_filter;
         sayts "pid_file=$pidfile";
         my $pid = $self->_find_pid( $pidfile );
         sayts "checking pid exists=$pid";
