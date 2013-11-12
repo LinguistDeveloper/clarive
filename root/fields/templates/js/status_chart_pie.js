80b19399741e6f43b8f9be74af4b4a14af0c8231
Raul Martin <raul.hernandez@vass.es>
2013-11-12 12:05:38 +0100

    nuevo fieldlet status pie

---
+++ root/fields/templates/js/status_chart_pie.js
@@ -0,0 +1,13 @@
+/*
+name: Status chart pie
+params:
+    origin: 'template'
+    html: '/fields/templates/html/status_chart_pie.html'
+    field_order: 1
+    section: 'details'
+---
+*/
+
+(function(){
+    return []
+})
