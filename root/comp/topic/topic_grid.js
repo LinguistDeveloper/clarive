b9e7ade4174137a427a6a0a995fe01ce343d3f3f
rodrigolive <rodrigolive@gmail.com>
2013-12-12 22:39:57 +0100

    no returnOpposite color in topic labels, just use white for now, desirable: set label with fore+back color

--- root/comp/topic/topic_grid.js
+++ root/comp/topic/topic_grid.js
@@ -574,8 +574,8 @@
                 var label_color = label[2];
                 tag_color_html = tag_color_html
                     //+ "<div id='boot'><span class='label' style='font-family:Helvetica Neue,Helvetica,Arial,sans-serif;font-size: xx-small; font-weight:bolder;float:left;padding:1px 4px 1px 4px;margin-right:4px;color:"
-                    + "<span style='font-family:Helvetica Neue,Helvetica,Arial,sans-serif;font-size: xx-small; font-weight:bolder;float:left;padding:1px 4px 1px 4px;margin-right:4px;-webkit-border-radius: 3px;-moz-border-radius: 3px;border-radius: 3px;color:" 
-                    + returnOpposite(label_color.substr(1)) + ";background-color:" + label_color + "'>" + label_name + "</span>";
+                    + "<span style='font-family:Helvetica Neue,Helvetica,Arial,sans-serif;font-size: xx-small; font-weight:bolder;float:left;padding:1px 4px 1px 4px;margin-right:4px;-webkit-border-radius: 3px;-moz-border-radius: 3px;border-radius: 3px;"
+                    + "color: #fff;background-color:" + label_color + "'>" + label_name + "</span>";
             }
         }
         
