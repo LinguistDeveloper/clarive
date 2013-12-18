151295fc2c0001c3f114424f7f18f89720c72515
Ricardo Martínez <ricardo.martinez@vass.es>
2013-12-19 00:49:36 +0100

    Added hostname to monitor_on.sh

--- bin/monitor_on.sh
+++ bin/monitor_on.sh
@@ -1,3 +1,4 @@
-sar -u 2 10000 > /tmp/cpu_$1.csv &
-sar -r 2 10000 > /tmp/mem_$1.csv &
-sar -n ALL 2 10000 > /tmp/net_$1.csv &
+SERVER=`hostname`
+sar -u 2 10000 > /tmp/cpu_$1_$SERVER.csv &
+sar -r 2 10000 > /tmp/mem_$1_$SERVER.csv &
+sar -n ALL 2 10000 > /tmp/net_$1_$SERVER.csv &
