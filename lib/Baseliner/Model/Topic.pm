5ea7cb7920a17fd1befad9670c9a4c0e7654c9cf
Ricardo Martínez <ricardo.martinez@vass.es>
2013-12-19 05:16:48 +0100

    Debug for create new topic 2

--- lib/Baseliner/Model/Topic.pm
+++ lib/Baseliner/Model/Topic.pm
@@ -2038,7 +2038,8 @@ sub set_release {
     $notify->{project} = \@projects if @projects;
 
     # check if arrays contain same members
-    _log "Guardando $release_field";
+    _log "Guardando $release_field: $old_release -> $new_release";
+
     if ( $new_release ne $old_release ) {
         if($release_row){
             my $rs = DB->BaliMasterRel->search({from_mid => $old_release, to_mid=>$topic_mid, rel_field => $release_field})->delete;
