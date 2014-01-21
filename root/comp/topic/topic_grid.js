afea30c9bfb56538c73f970b036da3537f7240a3
Raul Martin <raul.hernandez@vass.es>
2014-01-21 15:55:43 +0100

    modificacion parse fecha informes

--- root/comp/topic/topic_grid.js
+++ root/comp/topic/topic_grid.js
@@ -712,20 +712,18 @@
 			var res = str.replace('_' +  this.alias,"");
 			value = rec.json[res];
 		};			
-        if( !value ) return '';
+        if( !value && value == undefined ) return '';
 		//var value_to_date = new Date(value);
 		//return value_to_date.dateFormat('d/m/Y');
 		var date;
 		if (value.getMonth) {
 			date = value;
 		}else{
-			var dateStr= value; //returned from mysql timestamp/datetime field
-			if (value.split) { 
-				var a=dateStr.split(" ");
-				var d=a[0].split("-");
-				var t=a[1].split(":");
-				date = new Date(d[0],(d[1]-1),d[2],t[0],t[1],t[2]);
-			}else return ''
+			var dateStr= value;
+			var a=dateStr.split(" ");
+			var d=a[0].split("-");
+			var t=a[1].split(":");
+			date = new Date(d[0],(d[1]-1),d[2],t[0],t[1],t[2]);
 		}
 		return date.dateFormat('d/m/Y');
     };
