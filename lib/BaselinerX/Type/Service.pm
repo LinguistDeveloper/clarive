e0c253c2d43ea19de1a6f47e0bc8e71d6abf8bf7
rodrigolive <rodrigolive@gmail.com>
2014-02-13 11:59:04 +0100

    fix exec error in fake job in rules dsl

--- lib/BaselinerX/Type/Service.pm
+++ lib/BaselinerX/Type/Service.pm
@@ -191,6 +191,7 @@ package BaselinerX::Type::Service::Container::Job;
 use Baseliner::Moose;
 has job_dir => qw(is rw isa Str lazy 1), default=>sub{ Util->_tmp_dir() };
 has job_stash  => qw(is rw isa HashRef required 1 weak_ref 1);
+has exec => qw(is rw isa Num default 1);
 has backup_dir         => qw(is rw isa Any lazy 1), default => sub { 
     my ($self) = @_;
     return ''.Util->_file( $self->job_dir, '_backups' );
