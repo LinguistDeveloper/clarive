6688104b760d637a62e0cf06b0a8695fbd4ac11a
rodrigolive <rodrigolive@gmail.com>
2014-03-13 19:48:57 +0100

    service fake testing attributes used in DSL try

--- lib/BaselinerX/Type/Service.pm
+++ lib/BaselinerX/Type/Service.pm
@@ -192,6 +192,8 @@ use Baseliner::Moose;
 has job_dir => qw(is rw isa Str lazy 1), default=>sub{ Util->_tmp_dir() };
 has job_stash  => qw(is rw isa HashRef required 1 weak_ref 1);
 has exec => qw(is rw isa Num default 1);
+has bl => qw(is rw isa Str default TEST);
+has job_type => qw(is rw isa Str default promote);
 has backup_dir         => qw(is rw isa Any lazy 1), default => sub { 
     my ($self) = @_;
     return ''.Util->_file( $self->job_dir, '_backups' );
