649fc02d89df18da0028ac19e8f2e467fc343834
Ricardo Martinez <ricardo@clarive.com>
2014-03-18 17:02:10 +0100

    Fixed reload of new topic

--- lib/Baseliner/Model/Topic.pm
+++ lib/Baseliner/Model/Topic.pm
@@ -695,7 +695,7 @@ sub update {
                         category_name=>$category->{name}, 
                         notify_default=>\@users, subject=>$subject, notify=>$notify }   # to the event
                 });  
-                $return_options->{reload} = 1;                 
+                #$return_options->{reload} = 1;                 
             } 
             => sub { # catch
                 mdb->topic->remove({ mid=>"$topic_mid" },{ multiple=>1 });
