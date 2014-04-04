4feb251a77ee688620370b20219c480dc9c05181
raul <raul@clarive.com>
2014-04-04 15:47:02 -0100

    parse key

--- lib/BaselinerX/CI/report.pm
+++ lib/BaselinerX/CI/report.pm
@@ -764,7 +764,7 @@ method run( :$start=0, :$limit=undef, :$username=undef, :$query=undef, :$filter=
 	my %meta = map { $_->{id_field} => $_ } _array( Baseliner->model('Topic')->get_meta(undef, undef, $username) );  # XXX should be by category, same id fields may step on each other
 	my @selects = map { ( $_->{meta_select_id} // $select_field_map{$_->{id_field}} // $_->{id_field} ) => $_->{category} } _array($fields{select});
 
-    my %selects_ci_columns = map { ( $_->{meta_select_id} // $select_field_map{$_->{id_field}} // $_->{id_field} ) . '_' . Util->_unac($_->{category}) => $_->{ci_columns} } grep { exists $_->{ci_columns}} _array($fields{select});
+    my %selects_ci_columns = map { ( $_->{meta_select_id} // $select_field_map{$_->{id_field}} // $_->{id_field} ) . '_' . $_->{category} => $_->{ci_columns} } grep { exists $_->{ci_columns}} _array($fields{select});
 
 	#_log ">>>>>>>>>>>>>>>>>>>>>SELECT FIELDS: " . _dump @selects;
     _log ">>>>>>>>>>>>>>>>>>>>>SELECT FIELDS CI COLUMNS: " . _dump %selects_ci_columns;
@@ -1031,7 +1031,8 @@ method run( :$start=0, :$limit=undef, :$username=undef, :$query=undef, :$filter=
 
         while( my($k,$v) = each %row ) {
 
-            my $parse_key =  Util->_unac($k);
+            #my $parse_key =  Util->_unac($k);
+            my $parse_key =  $k;
 
             if ( exists $selects_ci_columns{$parse_key} ) {
                 _log ">>>>>>>>>>>>>>>>>>Existe CI:";
