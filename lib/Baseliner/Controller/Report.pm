003ba1eafbf35722358e70f26f387032b1515cc3
raul <raul@clarive.com>
2014-04-07 15:53:10 -0100

    new property collection_extends to get related data on reports

--- lib/Baseliner/Controller/Report.pm
+++ lib/Baseliner/Controller/Report.pm
@@ -15,15 +15,24 @@ $ENV{'NLS_DATE_FORMAT'} = 'YYYY-MM-DD HH24:MI:SS';
 sub get_ci_columns : Local {
     my ($self, $c) = @_;
     my $p = $c->req->params;
+
+    my $root = 'BaselinerX::CI::';
+    my $collection = $root . $p->{collection};
+    my $collection_extends = $root . $p->{collection_extends} if $p->{collection_extends} ;
+
+    my @columns;
+    my @columns_extends_ci;
+    my @columns_ci = map { +{ name => $_->name}} grep {$_->associated_class->get_meta_instance->_class_name eq $collection} $collection->meta->get_all_attributes;
     
-    my $collection = 'BaselinerX::CI::' . $p->{collection};
-    my @columns_ci = map { +{ name => $_->name} } grep {$_->associated_class->get_meta_instance->_class_name eq $collection} $collection->meta->get_all_attributes;
+    if ($collection_extends) {
+        my @columns_extends_ci = map { +{ name => $_->name}} grep {$_->associated_class->get_meta_instance->_class_name eq $collection_extends} $collection_extends->meta->get_all_attributes;
+        push @columns_ci, @columns_extends_ci;
+    }
 
+    
     $c->stash->{json} = { data => \@columns_ci, totalCount => scalar @columns_ci };
-
     $c->forward('View::JSON');
 }
-
 
 1;
 
