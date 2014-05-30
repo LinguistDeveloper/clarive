aba6f402f8ddbfc7a4909475ba18a04936839d6c
Ricardo Martinez <ricardo@clarive.com>
2014-05-30 11:07:29 +0200

    Cherry pick from 6.1

--- root/forms/topic_status.js
+++ root/forms/topic_status.js
@@ -2,7 +2,9 @@
     var data = params.data || {};
     return [
         { xtype:'textfield', fieldLabel: _('Topics'), name: 'topics', value: data.topics },
-        new Baseliner.ComboStatus({ name: 'new_status', fieldLabel: _('New Status'), value: data.new_status  })
+        { xtype:'textfield', fieldLabel: _('Old status'), name: 'old_status', value: data.old_status },
+        { xtype:'textfield', fieldLabel: _('New status'), name: 'new_status', value: data.new_status },
+        { xtype:'textfield', fieldLabel: _('User'), name: 'username', value: data.username }
     ]
 })
 
