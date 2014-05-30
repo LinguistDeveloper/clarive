2ade0feaaf06c63c41a5eee7a0f614c71122b997
Ricardo Martinez <ricardo@clarive.com>
2014-05-30 11:07:29 +0200

    Added username to change_status panel

--- root/forms/topic_status.js
+++ root/forms/topic_status.js
@@ -3,8 +3,8 @@
     return [
         { xtype:'textfield', fieldLabel: _('Topics'), name: 'topics', value: data.topics },
         { xtype:'textfield', fieldLabel: _('Old status'), name: 'old_status', value: data.old_status },
-        { xtype:'textfield', fieldLabel: _('New status'), name: 'new_status', value: data.new_status }
-        //{ xtype:'textfield', fieldLabel: _('User'), name: 'username', value: data.username }
+        { xtype:'textfield', fieldLabel: _('New status'), name: 'new_status', value: data.new_status },
+        { xtype:'textfield', fieldLabel: _('User'), name: 'username', value: data.username }
     ]
 })
 
