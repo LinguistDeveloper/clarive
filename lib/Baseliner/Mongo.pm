69bf83c415b5ca96fdee46a5d4148ed977f98466
Andrés Gómez <andres@gmail.com>
2014-06-10 14:24:59 -0100

    fixed warning in Mongo.pm on traying to access to an undefined env. var

--- lib/Baseliner/Mongo.pm
+++ lib/Baseliner/Mongo.pm
@@ -513,7 +513,8 @@ sub AUTOLOAD {
     my ($coll) = reverse( split(/::/, $name));
     Util->_fail('The method is `joins` not `join`') if $coll eq 'join';
     Util->_debug( "TRACE: $coll: ". join('; ',caller) ) if $ENV{CLARIVE_TRACE};
-    return ( ( $ENV{CLARIVE_TRACE}==2 && $coll ne 'cache' ) || $ENV{CLARIVE_TRACE}>2 )
+
+    return ( ( defined $ENV{CLARIVE_TRACE} && $ENV{CLARIVE_TRACE}==2 && $coll ne 'cache' ) || ( defined $ENV{CLARIVE_TRACE} && $ENV{CLARIVE_TRACE}>2 ))
           ? bless { coll=>$self->collection($coll) } => 'Baseliner::Mongo::TraceCollecion'
           : $self->collection( $coll )
 }
