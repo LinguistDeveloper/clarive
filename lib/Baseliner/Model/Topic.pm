ec7c0b09079ad7d27199628a7bea97a2e0b44052
Saul de Leon <saul@clarive.com>
2014-06-18 09:05:06 -0100

    change status fixed

--- lib/Baseliner/Model/Topic.pm
+++ lib/Baseliner/Model/Topic.pm
@@ -2600,7 +2600,7 @@ sub change_status {
     
     my $doc = mdb->topic->find_one({ mid=>"$mid" });
 
-    my $id_old_status = $p{id_old_status} || $doc->{category_status}{id};
+    my $id_old_status = $p{id_old_status} || $doc->{category_status}{id_status};
     my $status = $p{status} || $self->find_status_name($p{id_status});
     my $old_status = $p{old_status} || $self->find_status_name($id_old_status);
     my $callback = $p{callback};
@@ -2612,7 +2612,7 @@ sub change_status {
                 
                 _fail( _loc('Id not found: %1', $mid) ) unless $doc;
                 _fail _loc "Current topic status '%1' does not match the real status '%2'. Please refresh.", $doc->{category_status}{name}, $old_status 
-                    if $doc->{category_status}{id} != $id_old_status;
+                    if $doc->{category_status}{id_status} != $id_old_status;
                 # XXX check workflow for user?
                 # update mongo
                 my $modified_on = $doc->{modified_on};
