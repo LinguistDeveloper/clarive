8484ff7217199719f93bb0ecbc057828a1b51478
Ricardo <ricardo@clarive.com>
2014-08-01 19:48:29 -0100

    Adapt security for topic jobs to 6.1

--- lib/BaselinerX/CI/topic.pm
+++ lib/BaselinerX/CI/topic.pm
@@ -181,12 +181,10 @@ sub items {
 sub jobs {
     my ($self, $p )=@_;
     my @jobs = $self->parents( isa=>'job', %$p );
-    if( length $p->{username} ) {
-        my $has_permission = model->Permissions->user_has_action( username=> $p->{username}, action=>'action.job.monitor' );
-        my $is_root = Baseliner->model('Permissions')->is_root($p->{username});
-        if (!$has_permission && !$is_root){
-            @jobs = ();
-        }
+    my $is_root = Baseliner->model('Permissions')->is_root($p->{username});
+    my $has_permission = Baseliner->model('Permissions')->user_has_action( username=> $p->{username}, action=>'action.job.monitor' );
+    if (!$has_permission and !$is_root){
+        @jobs = ();
     }
     wantarray ? @jobs : \@jobs;
 }
