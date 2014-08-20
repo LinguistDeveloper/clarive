60834165b9a33b5b29c76676ca55f70a814877c2
Ricardo <ricardo@clarive.com>
2014-08-19 23:06:54 -0100

    Messages while report execution

--- root/comp/lifecycle/report_run.js
+++ root/comp/lifecycle/report_run.js
@@ -3,9 +3,11 @@
     var id_report = params.id_report;
 
     if ( id_report ) {
+        Baseliner.message(_("Wait while retrieving fields list and format for report %1", params.report_name));
         Baseliner.ci_call( id_report, 'selected_fields', { username : '<% $c->username %>'}, function(res){
             params.fields = res;
             Baseliner.add_tabcomp( '/comp/topic/topic_grid.js', _(params.report_name), params );
+            Baseliner.message(_("Fields retrieved.  Now wait while generating report %1", params.report_name));
         });
     }
 
