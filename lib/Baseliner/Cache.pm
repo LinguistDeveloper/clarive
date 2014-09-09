e1f84a9376455c29bf106287d737a4d6bd1b388e
rodrigolive <rodrigolive@gmail.com>
2014-09-09 12:37:46 +0200

    mongo 2.6 key size cannot be equal to 1024 apparently

--- lib/Baseliner/Cache.pm
+++ lib/Baseliner/Cache.pm
@@ -12,7 +12,7 @@ sub set {
         : Storable::freeze(bless( \$value => 'Cache::SV' )); 
     if( length($frozen) < 16777216){
         $key = JSON::XS->new->utf8->canonical->encode( $key ) if ref $key;
-        return if length $key > 1024;
+        return if length $key >= 1024;
         mdb->cache->update({ _id=>$key },{ _id=>$key,v=>(ref $value ? Storable::freeze($value) : undef) },{ upsert=>1 });
     }
 }
@@ -20,7 +20,7 @@ sub set {
 sub get {
     my ($self,$key)=@_;
     $key = JSON::XS->new->utf8->canonical->encode( $key ) if ref $key;
-    return undef if length $key > 1024;
+    return undef if length $key >= 1024;
     my $doc = mdb->cache->find_one({ _id=>$key },{ _id=>0 });
     return undef unless $doc;
     my $value = $doc->{v};
