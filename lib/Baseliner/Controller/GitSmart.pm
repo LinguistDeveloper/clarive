4651353867a2ae80cd1da652b6aea3b5ac705c5e
rodrigo <rodrigo@clarive.com>
2014-09-11 21:35:02 +0200

    git streaming cgi, fast and lean; corrected error handling. merged from 6.1 gitscm feature TODO: sha parsing is slow

--- lib/Baseliner/Controller/GitSmart.pm
+++ lib/Baseliner/Controller/GitSmart.pm
@@ -1,7 +1,7 @@
 package Baseliner::Controller::GitSmart;
 =head1 NAME
 
-Baseliner::Controller::GitSmart - interface to Git Smart HTTP
+BaselinerX::Controller::GitSmart - interface to Git Smart HTTP
 
 =head1 DESCRIPTION
 
@@ -14,7 +14,8 @@ use Baseliner::Sugar;
 use Try::Tiny;
 use Path::Class;
 
-BEGIN { extends 'Catalyst::Controller::WrapCGI' }
+#BEGIN { extends 'Catalyst::Controller::WrapCGI' }
+BEGIN { extends 'Catalyst::Controller' }
 
 sub begin : Private {  #TODO control auth here
      my ($self,$c) = @_;
@@ -80,25 +81,25 @@ sub git : Path('/git/') {
             $filepath_info = sprintf '/gitsmart/git/%s/%s', $repo_dir->basename, $rest_uri; #  FILEPATH_INFO: /gitsmart/git/VT-FILES.git/info/refs
             $path_info = sprintf '/%s/%s', $repo_dir->basename, $rest_uri; # PATH_INFO: /VT-FILES.git/info/refs
         } else {
-            $self->git_error( $c, 'internal error', _loc 'Could not parse git uri: %1', $c->req->uri );
+            $self->process_error( $c, 'internal error', _loc 'Could not parse git uri: %1', $c->req->uri );
             $c->response->status( 404 );
             return;
         }
         
         if( !defined $ci_prj ) {
-            $self->git_error( $c, 'internal error', _loc("Invalid project name %1", $project) );
+            $self->process_error( $c, 'internal error', _loc("Invalid project name %1", $project) );
             $c->response->status( 404 );
             return;
         }
         if( !defined $ci_repo ) {
-            $self->git_error( $c, 'internal error', _loc("Invalid repository name %1", $reponame) );
+            $self->process_error( $c, 'internal error', _loc("Invalid repository name %1", $reponame) );
             $c->response->status( 404 );
             return;
         }
         _log _loc "Git: User %1 access to project %2 (mid=%3) repository %4 (mid=%5)", $c->username, $project, $ci_prj->mid,
             $reponame, $ci_repo->mid;
     } else {
-        $self->git_error( $c, 'invalid repository', _loc("Invalid or unauthorized git repository path %1", $repopath ) );
+        $self->process_error( $c, 'invalid repository', _loc("Invalid or unauthorized git repository path %1", $repopath ) );
         $c->response->status( 401 );
         return;
     }
@@ -106,7 +107,7 @@ sub git : Path('/git/') {
     # Check permissions
     if( ! length $c->username ) {
         if( ! exists $c->req->params->{service} ) {  # first request has param 'service'
-            $self->git_error( $c, 'access denied', 
+            $self->process_error( $c, 'access denied', 
                     _loc("Authentication failed for user '%1': %2", $c->stash->{login}, $c->stash->{auth_message} ) 
                 );
             return;
@@ -118,7 +119,7 @@ sub git : Path('/git/') {
             $c->response->status( 401 );
             return;
         } else {
-            $self->git_error( $c, 'access denied', 
+            $self->process_error( $c, 'access denied', 
                     _loc("Authentication failed for user '%1': %2", $c->stash->{login}, $c->stash->{auth_message} ) 
                 );
             $c->response->status( 401 );
@@ -126,7 +127,7 @@ sub git : Path('/git/') {
         }
     } elsif( $ci_prj ) {
         if( !$ci_prj->user_has_action(username=>$c->username,action=>'action.git.repository_access') ) {
-            $self->git_error( $c, _loc('User: %1 does not have access to the project %2', $c->username, $project ) );
+            $self->process_error( $c, _loc('User: %1 does not have access to the project %2', $c->username, $project ) );
             return;
         } 
     }
@@ -162,11 +163,11 @@ sub git : Path('/git/') {
 
     # run cgi
     my ($cgi_msg,$cgi_ret) = ('',0);
-    $self->cgi_to_response($c, sub {
+    $self->wrap_cgi_stream($c, sub {
         #_debug \%ENV;
         #_debug $p;
-        my $prjr = _file $home;
-        $prjr = $prjr->dir . "";
+        # my $prjr = _file $home;
+        # $prjr = $prjr->dir . "";
         $ENV{GIT_HTTP_EXPORT_ALL} = 1;
         $ENV{GIT_PROJECT_ROOT} = "$home";
         $ENV{REMOTE_USER} ||= 'baseliner';
@@ -175,34 +176,133 @@ sub git : Path('/git/') {
         $ENV{FILEPATH_INFO} = $filepath_info if length $filepath_info; 
         $ENV{PATH_INFO} = $path_info if length $path_info; 
         
-        $cgi_ret = system $cgi;
-        $cgi_msg = $self->res_msg();
-        if ($? == -1) {
-            die "failed to execute CGI '$cgi': $!";
-        }
-        elsif ($? & 127) {
-            die sprintf "CGI '$cgi' died with signal %d, %s coredump",
-                ($? & 127),  ($? & 128) ? 'with' : 'without';
-        }
-        else {
-            my $exit_code = $? >> 8;
-            return 0 if $exit_code == 0;
-            die "CGI '$cgi' exited non-zero with: $exit_code";
-        }
+        $cgi_ret = $self->run_forked( $c, $cgi );   # TODO use system $cgi in CygWin?
+    },sub{
+       my $err = shift;
+       $self->process_error($c,'GIT ERROR', $err);
     }); 
 
-    _debug "GIT RET: " . $cgi_ret;
-    _debug "GIT CGI: " . $cgi_msg;
-    #contains nasty chars: _debug "GIT OUT: " . $c->res->body;
-
     # now gather commit info and event it
+    $self->event_this($c,$sha,$fullpath,$ref,$ref_prev,$repo);
+}
+
+sub run_forked {
+    my ($self,$c,$cgi) = @_;
+    
+    # fork a child
+    use POSIX ":sys_wait_h";
+    pipe( my $stdoutr, my $stdoutw );
+    pipe( my $stderrr, my $stderrw );
+    pipe( my $stdinr,  my $stdinw );
+    my $pid = fork();
+    _fail("fork failed: $!") unless defined $pid;
+
+    if ($pid == 0) { # child
+        local $SIG{__DIE__} = sub {
+            print STDERR @_;
+            exit(1);
+        };
+        close $stdoutr;
+        close $stderrr;
+        close $stdinw;
+        #require CGI::Emulate::PSGI;
+        #local %ENV = (%ENV, CGI::Emulate::PSGI->emulate_environment($env));
+        open( STDOUT, ">&=" . fileno($stdoutw) ) or _fail( "Cannot dup STDOUT: $!");
+        open( STDERR, ">&=" . fileno($stderrw) ) or _fail( "Cannot dup STDERR: $!");
+        open( STDIN, "<&=" . fileno($stdinr) ) or _fail( "Cannot dup STDIN: $!");
+        #chdir(File::Basename::dirname($cgi));
+        exec($cgi) or _fail( "exec: $!");
+        exit(2);
+    }
+    close $stdoutw;
+    close $stderrw;
+    close $stdinr;
+    
+    my $siz = 0;
+    # do some writing to process STDIN
+    { 
+        _debug( 'starting write' );
+        local $/ = \10_485_760;
+		if( my $fh = $c->req->body ) {
+			while( my $in = <$fh> ) { #<STDIN> ) {
+				_debug( sprintf 'write: %d (total=%s)', length $in, $siz+=length $in );
+				syswrite($stdinw, $in ) if length $in;
+            }
+        }
+    }
+    # close STDIN so child will stop waiting
+    close $stdinw;
+    
+    # now read git STDOUT
+    $siz = 0;
+    my $has_headers = 0;
+    while (waitpid($pid, WNOHANG) <= 0) {
+        my $out = do { local $/ = \10_485_760; <$stdoutr> } || '';
+        _debug( sprintf 'read: %d (total=%s)', length $out, $siz+=length $out );
+        if( !$has_headers && $out =~ m/^(.*?)\r\n\r\n(.*)$/s ) {
+            my $head = $1;
+            while( $head =~ m/^(.+): (.+)[\n\r]?/mg ) {
+                $c->res->headers->header( $1 => $2 );
+            }
+            $out = $2;
+            $has_headers = 1;
+            _debug( 'finhead: ' . length $head );
+            _debug( 'finread: ' . length $out );
+            $c->res->finalize_headers($c);
+        }
+        $c->res->write( $out ) if length $out;
+    }
+    my $out = do { local $/; <$stdoutr> } || '';
+    my $err = do { local $/; <$stderrr> } || '';
+    _error( "ERROR FINAL git cgi: $err" ) if length $err;
+    if( length $err ) {
+        _fail $err;
+    }
+    _debug( sprintf 'readfin: %d (total=%s)', length $out, $siz+=length $out );
+    $c->res->write( $out ) if length $out;
+    $c->res->body( '' );
+    return 0;
+}
+
+sub process_error {
+    my ($self,$c,$type,$err) = @_;
+    $err = _loc( "CLARIVE: %1: %2", $type, "\n$err\n" );
+    my ($git_ver) = $c->req->user_agent =~ /git\/(.+)$/;
+    $git_ver =~ s{\.GIT}{}g;  # ie. 2.1.0.GIT
+    my $service = $c->req->params->{service};
+    $c->session->{git_last_service} //= $service;
+    $service //= $c->session->{git_last_service} // 'git-upload-pack';
+    _debug ">>> GIT SERVICE=$service";
+    _debug ">>> GIT VERSION=$git_ver";
+    require version;
+    if( !length($git_ver) || version->parse($git_ver) < version->parse('1.8.3') ) {
+        _error( $err );
+        my $servtxt = sprintf "# service=%s\n",$service;
+        my $errtxt = sprintf "ERR %s\n",$err;
+        my $msg = sprintf "%04X%s0000%04X%s",4+length($servtxt),$servtxt,4+length($errtxt),$errtxt; #  "0036# service=git-receive-pack0000ERR This is a test\n";
+        $c->res->status( 200 );
+        $c->res->content_type( "application/x-$service-advertisement" );
+        $c->res->body( $msg );
+        return;
+    } else {
+        # 1.8.3 on can handle this error message style
+        $c->res->status( 500 );
+        $c->res->content_type( 'text/plain' );
+        $c->res->body( $err );
+        return;
+    }
+}
+
+
+sub event_this {
+    my ($self,$c,$sha,$fullpath,$ref,$ref_prev,$repo)=@_;
     try {
         if( $sha ) {  
             my $g = Girl::Repo->new( path=>"$fullpath" );
-            my $repo_doc = ci->GitRepository->find_one({ repo_dir=>qr/$fullpath/ });
+            my $repo_row = DB->BaliMaster->search( { collection => 'GitRepository', yaml => { like => "%repo_dir: ".$fullpath."%"} } )->first;
             my $repo_id;
-            if ($repo_doc) {
-                $repo_id = $repo_doc->{mid};
+            if ($repo_row) {
+                $repo_id = $repo_row->mid;
             } else {
                 master_new 'GitRepository' => {
                     name    => "$fullpath",
@@ -230,7 +330,7 @@ sub git : Path('/git/') {
                     => { username=>$c->username, repository=>$repo, message=>$title, commit=>$commit, diff=>$diff, branch=>$ref_short, ref=>$ref, sha=>$sha }
                     => sub { 
                         my $mid;
-                        my $master = ci->GitRevision->search_ci( sha=>$sha );
+                        my $master = DB->BaliMaster->search({ ns=>"git.revision/$sha" })->first;
                         if( ! $master ) {
                             my $commit2 = substr( $sha, 0, 7 );
                             my $msg = substr( $commit->message, 0, 15 );
@@ -257,8 +357,8 @@ sub git : Path('/git/') {
     } catch {
         _log "GIT: ERROR: Could not event git operation: " . shift();
     };
-
 }
+
 
 sub _format_diff {
     my ($diff, %p ) = @_;
@@ -278,6 +378,7 @@ sub res_msg {
     return $s;
 }
 
+# git_error deprecated, use process_error
 sub git_error {
     my ($self, $c, $msg, $err ) = @_;
     _error( _loc('Git error: %1 %2 (user=%3)', $msg, $err, $c->username ) );
@@ -286,4 +387,122 @@ sub git_error {
         $self->res_msg( sprintf("\nCLARIVE ERROR: %s",$msg), $err)
     );
 }
+
+# rgo: my own CGI wrapper:  TODO maybe a Catalyst module somewhere?
+use URI ();
+use URI::Escape;
+use HTTP::Request::Common;
+open my $REAL_STDIN, "<&=".fileno(*STDIN);
+open my $REAL_STDOUT, ">>&=".fileno(*STDOUT);
+ 
+sub wrap_cgi_stream {
+  my ($self, $c, $call, $call_err) = @_;
+  my $req = HTTP::Request->new(
+    map { $c->req->$_ } qw/method uri headers/
+  );
+  my $body = $c->req->body;
+  my $body_content = '';
+ 
+  $req->content_type($c->req->content_type); # set this now so we can override
+ 
+  if (!$body) { # Slurp from body filehandle
+    my $body_params = $c->req->body_parameters || {};
+ 
+    if (%$body_params) {
+      my $encoder = URI->new;
+      $encoder->query_form(%$body_params);
+      $body_content = $encoder->query;
+      $req->content_type('application/x-www-form-urlencoded');
+      $req->content($body_content);
+      $req->content_length(length($body_content));
+    }
+  }
+ 
+  my $username_field = $self->{CGI}{username_field} || 'username';
+  my $username = (($c->can('user_exists') && $c->user_exists)
+               ? eval { $c->user->obj->$username_field }
+                : '');
+  $username ||= $c->req->remote_user if $c->req->can('remote_user');
+ 
+  my $path_info = '/'.join '/' => map {
+    utf8::is_utf8($_) ? uri_escape_utf8($_) : uri_escape($_)
+  } @{ $c->req->args };
+ 
+  my $env = HTTP::Request::AsCGI->new(
+              $req,
+              ($username ? (REMOTE_USER => $username) : ()),
+              PATH_INFO => $path_info,
+# eww, this is likely broken:
+              FILEPATH_INFO => '/'.$c->action.$path_info,
+              SCRIPT_NAME => $c->uri_for($c->action, $c->req->captures)->path
+            );
+ 
+  {
+    my $saved_error;
+ 
+    local %ENV = %{ $self->_filtered_env(\%ENV) };
+ 
+    $env->setup;
+    eval { $call->() };
+    $saved_error = $@;
+    #$env->restore;
+ 
+    if( $saved_error ) {
+        if( $call_err ) {
+            $call_err->($saved_error);
+        } else {
+            die $saved_error if ref $saved_error;
+            Catalyst::Exception->throw(
+                message => "CGI invocation failed: $saved_error"
+               );
+        }
+    }
+  }
+ 
+  #return $env->response;
+ return ;
+}
+my $DEFAULT_KILL_ENV = [qw/
+  MOD_PERL SERVER_SOFTWARE SERVER_NAME GATEWAY_INTERFACE SERVER_PROTOCOL
+  SERVER_PORT REQUEST_METHOD PATH_INFO PATH_TRANSLATED SCRIPT_NAME QUERY_STRING
+  REMOTE_HOST REMOTE_ADDR AUTH_TYPE REMOTE_USER REMOTE_IDENT CONTENT_TYPE
+  CONTENT_LENGTH HTTP_ACCEPT HTTP_USER_AGENT
+/];
+ 
+sub _filtered_env {
+  my ($self, $env) = @_;
+  my @ok;
+ 
+  my $pass_env = $self->{CGI}{pass_env};
+  $pass_env = []            if not defined $pass_env;
+  $pass_env = [ $pass_env ] unless ref $pass_env;
+ 
+  my $kill_env = $self->{CGI}{kill_env};
+  $kill_env = $DEFAULT_KILL_ENV unless defined $kill_env;
+  $kill_env = [ $kill_env ]  unless ref $kill_env;
+ 
+  if (@$pass_env) {
+    for (@$pass_env) {
+      if (m!^/(.*)/\z!) {
+        my $re = qr/$1/;
+        push @ok, grep /$re/, keys %$env;
+      } else {
+        push @ok, $_;
+      }
+    }
+  } else {
+    @ok = keys %$env;
+  }
+ 
+  for my $k (@$kill_env) {
+    if ($k =~ m!^/(.*)/\z!) {
+      my $re = qr/$1/;
+      @ok = grep { ! /$re/ } @ok;
+    } else {
+      @ok = grep { $_ ne $k } @ok;
+    }
+  }
+  return { map {; $_ => $env->{$_} } @ok };
+}
+
 1;
