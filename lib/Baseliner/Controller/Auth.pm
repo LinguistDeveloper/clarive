90157382e66df6e6105ab8e266c2d09cb6e52ae0
Ricardo <ricardo@clarive.com>
2014-09-30 19:17:07 -0100

    Register auth events out of the controller

--- lib/Baseliner/Controller/Auth.pm
+++ lib/Baseliner/Controller/Auth.pm
@@ -14,14 +14,6 @@ register 'action.change_password' => {
     name => 'User can change his password',
 };
 
-register 'event.auth.logout' => { name => 'User Logout' } ;
-register 'event.auth.ok' => { name => 'Login Ok' } ;
-register 'event.auth.failed' => { name => 'Login Failed' } ;
-register 'event.auth.saml_ok' => { name => 'Login by SAML Ok' } ;
-register 'event.auth.saml_failed' => { name => 'Login by SAML Failed' } ;
-register 'event.auth.surrogate_ok' => { name => 'Surrogate Ok' } ;
-register 'event.auth.surrogate_failed' => { name => 'Surrogate Failed' } ;
-
 register 'config.login' => {
     metadata => [
         { id => 'delay_attempts', default => 5, name=> 'Number of attempts the user ' },
