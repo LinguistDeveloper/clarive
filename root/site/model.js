d8ee43abcaae5af97ac603d58254a6dc25738ce7
Beatriz Muñoz <beatriz@clarive.com>
2014-10-28 07:13:29 -0100

    Revert commit b2a9195c7a9441d6179c0f3259a50bf13bdf9758. It should be put again

--- root/site/model.js
+++ root/site/model.js
@@ -2212,8 +2212,8 @@ Baseliner.MetaForm = Ext.extend( Ext.Panel, {
         else if( meta.type == 'ci' ) {
             var bp = { _whoami:'MetaForm', bl: bl };
             if( meta.role && meta.role != 'CI' ) bp.role = meta.role;
+            else if( meta.classname ) bp.classname = meta.classname;
             else bp.role = 'CI';  // avoids a bad store call
-            if( meta.classname ) bp.classname = meta.classname;
             var store = new Baseliner.store.CI({ baseParams: bp });
             store.on('load', function(){
                 if( self.data && self.data[id]!== undefined )
