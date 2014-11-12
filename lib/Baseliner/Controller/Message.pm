75a553f07a977700c80ce4e94d40f0315a9cd57a
rodrigo <rodrigo@clarive.com>
2014-11-12 12:33:40 +0100

    sms indexes with auto expiry after 31 days so that messages do not linger

--- lib/Baseliner/Controller/Message.pm
+++ lib/Baseliner/Controller/Message.pm
@@ -197,6 +197,7 @@ sub sms_create : Local {
     my $p = $c->req->params;
     try {
         model->Permissions->user_has_action( username=>$c->username, action=>'action.admin.sms' ) || _fail _loc 'Unauthorized';
+        $self->sms_set_indexes;
         my $title = $p->{title} || _fail _loc 'Missing message title';
         my $text = $p->{text} || _fail _loc 'Missing message text';
         my $more = $p->{more} || '';
@@ -214,6 +215,21 @@ sub sms_create : Local {
         $c->stash->{json} = { success => \0, msg => $err };
     };
     $c->forward('View::JSON');
+}
+
+sub sms_set_indexes : Private {
+    my $expire_secs = 2_764_800;  # 32 days, 1 day above max system message of 30 days
+    if( scalar mdb->sms->get_indexes < 4 ) {   # works if the collection does not exist or missing indexes
+        mdb->sms->drop_indexes;
+        Util->_debug( 'Creating mongo sms indexes. Expire seconds=' . $expire_secs ); 
+        my $coll = mdb->db->get_collection('sms');
+        $coll->ensure_index($_,{ background=>1 }) for ({ _id=>1 }, { username=>1 },{ expires=>1 });
+        $coll->ensure_index({ t=>1 },{ expire_after_seconds=>$expire_secs }); # 1 month max
+    }
+    else {
+        # reconfigure expire seconds
+        mdb->sms->run_command([ collMod=>"sms", index=>{keyPattern=>{t=>1}, expireAfterSeconds=>$expire_secs }]);
+    }
 }
 
 sub sms_del : Local {
