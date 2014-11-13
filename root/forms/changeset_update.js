040d2fd251477d67ae6919af6e0dd011f103d803
Guillermo Reviejo <guillermo@clarive.com>
2014-11-13 09:36:54 -0100

    new checkbox for ignore finished statuses on Update Changesets

--- root/forms/changeset_update.js
+++ root/forms/changeset_update.js
@@ -4,6 +4,7 @@
         new Baseliner.ComboStatus({ name: 'status_on_ok', fieldLabel: _('Status on Success'), value: data.status_on_ok||''  }),
         new Baseliner.ComboStatus({ name: 'status_on_fail', fieldLabel: _('Status on Failure'), value: data.status_on_fail||''  }),
         new Baseliner.ComboStatus({ name: 'status_on_rollback_ok', fieldLabel: _('Status on Rollback OK'), value: data.status_on_rollback_ok||''  }),
-        new Baseliner.ComboStatus({ name: 'status_on_rollback_fail', fieldLabel: _('Status on Rollback Failure'), value: data.status_on_rollback_fail||''  })
+        new Baseliner.ComboStatus({ name: 'status_on_rollback_fail', fieldLabel: _('Status on Rollback Failure'), value: data.status_on_rollback_fail||''  }),
+        new Ext.form.Checkbox({ name: 'status_ignore_closed', fieldLabel: _('Ignore finished statuses'), checked: data.status_ignore_closed })
     ]
 })
