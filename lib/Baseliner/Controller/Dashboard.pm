75ea75c48b353e446ba6eadf2614a697d2736c67
Ricardo <ricardo@clarive.com>
2014-11-26 13:15:11 -0100

    Use vars instead of load data in last change status

--- lib/Baseliner/Controller/Dashboard.pm
+++ lib/Baseliner/Controller/Dashboard.pm
@@ -1238,7 +1238,7 @@ sub list_status_changed: Local{
     my @mid_topics;
     my @topics = mdb->activity->find($query)->sort({ ts=>-1 })->all;
     map {
-        my $ed = _load( $_->{event_data} );
+        my $ed = $_->{vars} ;
         if ( (exists $my_topics{$ed->{topic_mid}} || Baseliner->model("Permissions")->is_root( $c->username ) ) && $ed->{old_status} ne $ed->{status}){
             push @status_changes, { old_status => $ed->{old_status}, status => $ed->{status}, username => $ed->{username}, when => $_->{ts}, mid => $ed->{topic_mid} };
             push @mid_topics, $ed->{topic_mid};
