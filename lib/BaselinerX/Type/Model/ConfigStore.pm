d5809d752cc4ab9e581dde120a4967ac9c9b7225
Ricardo <ricardo@clarive.com>
2014-11-29 02:01:38 -0100

    Keep label in db saved config keys

--- lib/BaselinerX/Type/Model/ConfigStore.pm
+++ lib/BaselinerX/Type/Model/ConfigStore.pm
@@ -302,7 +302,7 @@ sub search {
     my @rows;
     while( my $r = $rs->next ) {
             my $config = $self->config_for_key( $r->{key} ) or warn 'No config for ' . $r->{key};
-            my $metadata = { type=>'?', default=>'', label=>$r->{key} };  # default values
+            my $metadata = { type=>'?', default=>'', label=>$r->{label} };  # default values
             if( $config ) {
                 try { $metadata = $config->metadata_for_key( $r->{key} ) or warn 'No metadata for ' . $r->{key}; } catch {_log $r->{key};};
             }
@@ -437,6 +437,9 @@ sub set {
     _throw 'Missing parameter key' unless $p{key};
     _throw 'Missing parameter value' unless defined $p{value};
 
+    my $registry_data = $self->search_registry( query=> $p{key} );
+    my ($original) = _array($registry_data->{data} // []) if $registry_data->{config_key} eq $p{key};
+    
     $self->delete( %p );
     
     my $row = mdb->config->insert(
@@ -444,7 +447,8 @@ sub set {
             value   => $p{value}, 
             ns      => $ns, 
             bl      => $bl,
-            ts      => mdb->ts
+            ts      => mdb->ts,
+            label   => $original->{config_label}
         }
     );
     return $row;
