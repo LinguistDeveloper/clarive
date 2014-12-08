7957bef352cf55639ded5e8b2ec5278c541fa692
rodrigo <rodrigo@clarive.com>
2014-12-08 18:41:24 +0100

    field metadata editable calculated from js field

--- lib/Baseliner/Model/Topic.pm
+++ lib/Baseliner/Model/Topic.pm
@@ -1273,6 +1273,7 @@ sub get_meta {
                 $d->{default_value} = eval $1;
             }
             $d->{field_order} //= 1;
+            $d->{editable} //= length $d->{js} ? 1 : 0;
             $d->{meta_type} = 'history' if $d->{js} && $d->{js} eq '/fields/templates/js/status_changes.js';  # for legacy only
             $d->{meta_type} ||= $d->{set_method} 
                 ? ($meta_types{ $d->{set_method} } // _fail("Unknown set_method $d->{set_method} for field $d->{name_field}") ) 
