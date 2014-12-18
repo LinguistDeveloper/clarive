54da488d622e3faf5cb31ae3d8639fe209cf5e63
Ricardo <ricardo@clarive.com>
2014-12-17 23:49:21 -0100

    fix: use Baseliner.HtmlEditor instead of htmleditor in sed notification

--- root/forms/notify_create.js
+++ root/forms/notify_create.js
@@ -4,6 +4,6 @@
         new Baseliner.UserAndRoleBox({ fieldLabel: _('TO'), name:'to', allowBlank: false, value: data.to }),
         new Baseliner.UserAndRoleBox({ fieldLabel: _('CC'), name:'cc', allowBlank: true, value: data.cc }),
         { xtype: 'textfield', fieldLabel: _('Subject'), name:'subject', anchor:'100%', allowBlank: false, value: data.subject },
-        { xtype: 'htmleditor', fieldLabel: _('Body'), name:'body', anchor:'100%', allowBlank: false, value: data.body }
+        new Baseliner.HtmlEditor({ fieldLabel: _('Body'), name:'body', anchor:'100%', allowBlank: false, value: data.body })
     ]
 })
