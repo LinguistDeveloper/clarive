ca68624e34b851c2c30512a66daf16cc9d618398
rodrigo <rodrigo@clarive.com>
2015-01-29 17:35:20 +0100

    fix: step error in container job, now step is always "RUN" for DSL jobs

--- lib/BaselinerX/Type/Service.pm
+++ lib/BaselinerX/Type/Service.pm
@@ -193,6 +193,7 @@ has job_dir => qw(is rw isa Str lazy 1), default=>sub{ Util->_tmp_dir() };
 has job_stash  => qw(is rw isa HashRef required 1 weak_ref 1);
 has exec => qw(is rw isa Num default 1);
 has bl => qw(is rw isa Str default TEST);
+has step => qw(is rw isa Str default RUN);
 has job_type => qw(is rw isa Str default promote);
 has backup_dir         => qw(is rw isa Any lazy 1), default => sub { 
     my ($self) = @_;
