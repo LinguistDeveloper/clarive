f2f9a2b36298c6e5ce84d17999f74d0ff7a143d4
Guillermo Reviejo <guillermo@clarive.com>
2015-02-19 15:59:46 -0100

    fix: (#19747) remove id_status comparation from get_user_friends in topic model

--- lib/Baseliner/Model/Topic.pm
+++ lib/Baseliner/Model/Topic.pm
@@ -2992,11 +2992,8 @@ sub get_users_friend {
 
     my @users;
     my $mid = $p{mid};
-    my @roles = _unique map { $_->{id_role} } grep { $$_{id_status_from} == $p{id_status} } _array(
-        mdb->category->find_one( 
-            { id => ''.$p{id_category} },
-            { workflow=>1 })->{workflow}
-    );
+    #my @roles = _unique map { $_->{id_role} } grep { $$_{id_status_from} == $p{id_status} } _array(mdb->category->find_one({ id => ''.$p{id_category} })->{workflow});
+    my @roles = _unique map { $_->{id_role} } _array(mdb->category->find_one({ id => ''.$p{id_category} })->{workflow});
     if (@roles){
         @users = Baseliner->model('Users')->get_users_from_mid_roles( mid => $mid, roles => \@roles);
         @users = _unique @users;
