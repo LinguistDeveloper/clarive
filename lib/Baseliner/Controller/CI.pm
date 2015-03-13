75d2a4935b59a321f4a662c52eeba710963030f8
rodrigo <rodrigo@clarive.com>
2015-03-13 11:57:19 +0100

    fix: broke icons again, class is not ref

--- lib/Baseliner/Controller/CI.pm
+++ lib/Baseliner/Controller/CI.pm
@@ -324,7 +324,7 @@ sub tree_objects {
             classname         => $row_class || '', 
             collection        => $row->{collection},
             moniker           => $row->{moniker},
-            icon              => ( $icons{ $row_class } // ( $icons{$row_class} = ref $row_class ? $row_class->icon : $generic_icon ) ),
+            icon              => ( $icons{ $row_class } // ( $icons{$row_class} = $row_class ? $row_class->icon : $generic_icon ) ),
             ts                => $row->{ts},
             bl                => $row->{bl},
             description       => $data->{description} // '',
