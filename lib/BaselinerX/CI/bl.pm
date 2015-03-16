7a89d0b0a4133331c384abf5f4b415d498e480be
rodrigo <rodrigo@clarive.com>
2015-03-16 13:33:44 +0100

    fix: rename all CIs variables bl when renaming a bl

--- lib/BaselinerX/CI/bl.pm
+++ lib/BaselinerX/CI/bl.pm
@@ -16,4 +16,17 @@ before save => sub {
     $self->moniker( $self->bl );
 };
     
+after save_data => sub {
+    my ($self, $master_row, $data, $opts, $old ) = @_;
+    # update statuses in topics
+    if( $$opts{changed}{bl} && defined $$old{bl} ) {
+        mdb->master_doc->update({ bl=>$$old{bl} },{ '$set'=>{ bl=>$self->bl } });
+        mdb->topic->update({ bl=>$$old{bl} },{ '$set'=>{ bl=>$self->bl } });
+        mdb->master_doc->update({ 'variables.'.$$old{bl} =>{'$exists'=>1} },
+        { '$rename'=>{ 'variables.'.$$old{bl} =>'variables.'.$self->bl } },
+        { multiple=>1 });
+        cache->clear;
+    }
+};
+
 1;
