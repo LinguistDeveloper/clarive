dd3c2579d8a47bd274b9bfb20f02e29b589ed55a
Ricardo <ricardo@clarive.com>
2015-03-18 09:54:36 -0100

    new: remove JES data in purge

--- lib/BaselinerX/Service/Purge.pm
+++ lib/BaselinerX/Service/Purge.pm
@@ -126,6 +126,11 @@ sub run_once {
                         _log "\tDeleting field data of ".$job->{mid}."....";
                         $data = $query->{data};
                         mdb->grid->delete($data);
+                    } else {
+                        if ( $actual->{more} eq 'jes' ) {
+                            _log "\tRemoving jes data of ".$job->{mid}."....";
+                            mdb->jes_log->remove({ id_log => 0+$actual->{id}});
+                        }
                     }
                 }
             } elsif( !$job->{purged} ) {
