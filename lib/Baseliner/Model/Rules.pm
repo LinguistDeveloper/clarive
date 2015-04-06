a987d5d5161a306ea1da35c23a7d5d47fddce4d6
Guillermo Reviejo <guillermo@clarive.com>
2015-04-06 14:56:18 +0200

    fix: (21674) fix accent in try dsl_build

--- lib/Baseliner/Model/Rules.pm
+++ lib/Baseliner/Model/Rules.pm
@@ -275,6 +275,7 @@ sub dsl_build {
     my $spaces = sub { '   ' x $_[0] };
     my $level = 0;
     local $Data::Dumper::Terse = 1;
+    local $Data::Dumper::Deparse = 1;
     for my $s ( _array $stmts ) {
         local $p{no_tidy} = 1; # just one tidy is enough
         my $children = $s->{children} || {};
