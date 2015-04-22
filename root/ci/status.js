7bd9adbfbc35fce8989c7b9a91c6328a6c48dd3b
Saul de Leon <saul@clarive.com>
2015-04-22 09:35:01 +0200

    Remove not used options in status creation form

--- root/ci/status.js
+++ root/ci/status.js
@@ -14,9 +14,11 @@
         bls,
         { xtype:'textfield', name: 'seq', fieldLabel:_('Sequence'), anchor:'100%', value: params.rec.seq==undefined ? 100 : params.rec.seq },
         { xtype: 'cbox', colspan: 1, fieldLabel: _('Bind releases'), name:'bind_releases', checked: bind_releases, allowBlank: true },
+        /* Demo options, not used
         { xtype: 'cbox', colspan: 1, fieldLabel: _('CI update'), name:'ci_update', checked: ci_update, allowBlank: true },
         { xtype: 'cbox', colspan: 1, fieldLabel: _('Frozen'), name:'frozen', checked: frozen, allowBlank: true },
         { xtype: 'cbox', colspan: 1, fieldLabel: _('Read only'), name:'readonly', checked: readonly, allowBlank: true },
+        */
         { xtype: 'cbox', colspan: 1, fieldLabel: _('View in tree'), name:'view_in_tree', checked: view_in_tree, allowBlank: true },
         {
             xtype: 'radiogroup',
@@ -33,8 +35,8 @@
             ]
         },
         { xtype:'textfield', name: 'color', fieldLabel:_('Color'), anchor:'100%', value: params.rec.color||'' },
-        { xtype:'textfield', name: 'max_inactivity_time', fieldLabel:_('Max inactivity time'), anchor:'20%', value: params.rec.max_inactivity_time||'0H' },
-        { xtype:'textfield', name: 'max_time_in_status', fieldLabel:_('Max time in status'), anchor:'20%', value: params.rec.max_time_in_status||'0H' },
+        //{ xtype:'textfield', name: 'max_inactivity_time', fieldLabel:_('Max inactivity time'), anchor:'20%', value: params.rec.max_inactivity_time||'0H' },
+        //{ xtype:'textfield', name: 'max_time_in_status', fieldLabel:_('Max time in status'), anchor:'20%', value: params.rec.max_time_in_status||'0H' },
         { xtype:'textfield', name: 'status_icon', fieldLabel:_('Icon Path'), anchor:'100%', 
             value: params.rec.status_icon||'' }
     ]
