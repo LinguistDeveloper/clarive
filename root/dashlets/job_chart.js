e8e81dd3c60105057682cc3d7e6af7fb916b4efa
Ricardo <ricardo@clarive.com>
2015-05-11 13:58:14 -0100

    enh: show values in pie and donut charts.  Pct in tooltip

--- root/dashlets/job_chart.js
+++ root/dashlets/job_chart.js
@@ -46,11 +46,48 @@
                          }
                        }
                      },
+                     pie: {
+                         label: {
+                             format: function (value, ratio, id) {
+                                 return value;
+                             }
+                         }
+                     },
                      donut: {
+                         label: {
+                             format: function (value, ratio, id) {
+                                 return value;
+                             }
+                         },
                          title: title
                      },
                      tooltip: {
-                        grouped: false // Default true
+                        grouped: false,
+                        contents: function (d, defaultTitleFormat, defaultValueFormat, color) {
+                            var $$ = this, config = $$.config,
+                                titleFormat = config.tooltip_format_title || defaultTitleFormat,
+                                nameFormat = config.tooltip_format_name || function (name) { return name; },
+                                valueFormat = config.tooltip_format_value || defaultValueFormat,
+                                text, i, title, value, name, bgcolor;
+                            for (i = 0; i < d.length; i++) {
+                                if (! (d[i] && (d[i].value || d[i].value === 0))) { continue; }
+
+                                if (! text) {
+                                    title = titleFormat ? titleFormat(d[i].x) : d[i].x;
+                                    text = "<table class='" + $$.CLASS.tooltip + "'>" + (title || title === 0 ? "<tr><th colspan='2'>" + title + "</th></tr>" : "");
+                                }
+
+                                name = nameFormat(d[i].name);
+                                value = valueFormat(d[i].value, d[i].ratio, d[i].id, d[i].index);
+                                bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : color(d[i].id);
+
+                                text += "<tr class='" + $$.CLASS.tooltipName + "-" + d[i].id + "'>";
+                                text += "<td class='name'><span style='background-color:" + bgcolor + "'></span>&nbsp;" + name + "</td>";
+                                text += "<td class='value'>&nbsp;" + d[i].value + ' (' + Math.round(d[i].ratio*100) + "%)</td>";
+                                text += "</tr>";
+                            }
+                            return text + "</table>";
+                        }
                      }
                 });
             });
