f1b2781e2645d51dc30d14a6c19a461ac2ab6547
Ricardo <ricardo@clarive.com>
2015-05-11 09:16:08 -0100

    enh: wrap columns in last_jobs

--- root/dashlets/last_jobs.js
+++ root/dashlets/last_jobs.js
@@ -10,7 +10,7 @@ my $iid = Util->_md5;
     Cla.ajax_json('/dashboard/last_jobs', { bls: bls }, function(res){
         var html = '<style>#boot .pagination a {line-height: 22px;} #boot .table td {padding: 3px} #boot .table th {padding: 3px}  #boot select {width: 60px;  height: 20px;line-height: 20px;} #boot input {width: 100px;height: 20px;padding:0px} #boot .pagination a {float: left;padding: 0 5px;}</style>';
         var div = document.getElementById(id);
-        html = html + '    <table class="table display nowrap stripe order-column compact" style="font-size: 85%;width: 100%" id="<% $iid %>">';
+        html = html + '    <table class="table display stripe order-column compact" style="font-size: 85%;width: 100%" id="<% $iid %>">';
         html = html + '        <thead>';
         html = html + '            <tr>';
         html = html + '                <th>'+_("Project")+'</th>';
