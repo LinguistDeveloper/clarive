d4c4facb8ae1d3f199bd4b0533a29f2b86c604ff
Elena Gomez <elena@clarive.com>
2015-05-19 13:25:53 -0100

    enh: new icons for clarive 6.3. Miscellaneus

--- root/site/explorer.js
+++ root/site/explorer.js
@@ -581,7 +581,7 @@ Baseliner.Explorer = Ext.extend( Ext.Panel, {
 
         var button_ci = new Ext.Button({
             cls: 'x-btn-icon',
-            icon: '/static/images/icons/ci-grey.png',
+            icon: '/static/images/icons/class.gif',
             handler: function(){
                 this.disable();
                 var that = this;
@@ -601,7 +601,7 @@ Baseliner.Explorer = Ext.extend( Ext.Panel, {
         
         var button_releases = new Ext.Button({
             cls: 'x-btn-icon',
-            icon: '/static/images/icons/release.gif',
+            icon: '/static/images/icons/release_explorer.png',
             handler: function(){
                 this.disable();
                 var that = this;
