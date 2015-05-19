a1ae0cfac5103cc4910905ab4c11f8b13684b91c
rodrigo <rodrigo@clarive.com>
2015-05-19 18:59:41 +0200

    enh: improve worker compatibility with windows, fix double tee output merging and server configuration value that was not working

--- lib/Baseliner/Role/ErrorThrower.pm
+++ lib/Baseliner/Role/ErrorThrower.pm
@@ -3,9 +3,9 @@ use Moose::Role;
 
 # error control 
 has throw_errors => qw(is rw isa Bool default 0 lazy 1);
-has ret          => qw(is rw isa Any), default => '';
-has rc           => qw(is rw isa Maybe[Num] default 0);
-has output       => qw(is rw isa Maybe[Str] lazy 1), default=>sub{
+has ret          => qw(is rw isa Any), default => '';   # return value/hash from command
+has rc           => qw(is rw isa Maybe[Num] default 0); # return code, 0 is ok, !0 bad
+has output       => qw(is rw isa Maybe[Str] lazy 1), default=>sub{   # stdout + stderr
     my $self = shift;
     return $self->ret;
 };
@@ -16,9 +16,7 @@ sub _throw_on_error {
     my $self = shift;
     return unless $self->throw_errors;
     use Baseliner::Utils;
-    _throw sprintf '%s: %s', $self->error, $self->ret if $self->rc;
+    _throw sprintf '%s: %s', $self->error, $self->output if $self->rc;
 }
-
-#sub output { shift->ret }
 
 1;
