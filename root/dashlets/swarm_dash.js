cdf5d4daa13cd1bed478cdcee178f9cc209bd097
rodrigo <rodrigo@clarive.com>
2015-05-22 15:18:05 +0200

    enh: swarm initial version

---
+++ root/dashlets/swarm_dash.js
@@ -0,0 +1,11 @@
+(function(params){ 
+    var id = params.id_div;
+    var meta = params.data;
+    require(['/site/swarm.js?'+Math.random()], function(res){
+        var div = document.getElementById(id);
+        div.innerHTML = '';
+        var config = Ext.apply({ start_mode: 'auto', height: div.offsetHeight, width: div.offsetWidth, renderTo: id }, meta);
+        var swarm = new Cla.Swarm(config);
+    });
+});
+
