7662e05011af154c292f56a7176a5957c68f96d3
Ricardo <ricardo@clarive.com>
2015-06-11 02:21:15 +0200

    fix: bl instead of name in job_distribution dashlet

--- lib/Baseliner/Controller/Job.pm
+++ lib/Baseliner/Controller/Job.pm
@@ -730,9 +730,9 @@ sub burndown_new : Local {
         my $yesterday = substr($now - $period, 0, 10);
         my $where = { starttime => { '$gt' => $yesterday } };
 
-        my @all_bls = map {$_->{name} } grep { $_->{bl} ne '*'} ci->bl->find()->all;
+        my @all_bls = map {$_->{bl} } grep { $_->{bl} ne '*'} ci->bl->find()->all;
         if ( _array($bls) ) {
-            @all_bls = map {$_->{name}} ci->bl->find({mid=>mdb->in(_array($bls))})->all;
+            @all_bls = map {$_->{bl}} ci->bl->find({mid=>mdb->in(_array($bls))})->all;
             $where->{bl} = mdb->in(@all_bls);
             _warn $bls;
         }
