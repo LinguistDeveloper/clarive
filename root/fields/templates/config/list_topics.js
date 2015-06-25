e838e10a1f1b6041a4a36c579c9f40034d2958d6
Ricardo <ricardo@clarive.com>
2015-06-25 09:26:04 +0200

    enh: height in list_topic components

--- root/fields/templates/config/list_topics.js
+++ root/fields/templates/config/list_topics.js
@@ -19,6 +19,7 @@
     ret.push([ 
     	{ xtype:'hidden', name:'fieldletType', value: 'fieldlet.system.list_topics' },
         { xtype:'textfield', fieldLabel: _('List of columns to show in grid'), name: 'columns', value: data.columns },
+        { xtype:'numberfield', fieldLabel: _('Height of grid in edit mode'), name: 'height', value: data.height || 250 },
     	{ xtype:'numberfield', name:'page_size', fieldLabel: _('Page size'), value: data.page_size },
     	{ xtype:'textfield', name:'parent_field', fieldLabel: _('Parent field'), value: data.parent_field },
         combo_datatable
