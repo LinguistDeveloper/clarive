76e2164346b03dcd52853c4a8c7d7458d4f682b5
Ricardo <ricardo@clarive.com>
2015-07-07 02:23:26 +0200

    enh: added configuration "show real dates" in history fields

--- root/fields/templates/config/status_changes.js
+++ root/fields/templates/config/status_changes.js
@@ -13,7 +13,19 @@
         if(element.name == 'hidden') { element.hide() };
         if(element.name == 'allowBlank') { element.hide() };
     });
-    ret.push([ 
+    var checkbox_show_real_dates = new Baseliner.CBox({
+        name: 'show_real_dates',
+        checked: data && data[ 'show_real_dates' ]=='1'  ? true : false,
+        default_value: true,
+        anchor: '100%',
+        width: '100%',
+        // hideLabel: true,
+        fieldLabel: _('Show real dates instead of "ago" format'),
+        style: 'margin-bottom: 3px'
+    });
+
+    ret.push([
+        checkbox_show_real_dates,
         { xtype:'hidden', name:'fieldletType', value: 'fieldlet.status_changes' } 
     ]);
     return ret;
