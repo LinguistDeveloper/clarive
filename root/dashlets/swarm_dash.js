eae286716eceb2720363bcbdd08bd019b3887eff
rodrigo <rodrigo@clarive.com>
2015-09-10 16:19:50 +0200

    fix: require error msg in swarm dashlet

--- root/dashlets/swarm_dash.js
+++ root/dashlets/swarm_dash.js
@@ -3,11 +3,11 @@
     var project_id = params.project_id;
     var meta = params.data;
 
-    require(['/site/swarm.js?'+Math.random()], function(res){
+    Cla.use(['/site/swarm.js'], function(res){
         var div = document.getElementById(id);
         div.innerHTML = '';
         var config = Ext.apply({ project_id: project_id, start_mode: 'auto', height: div.offsetHeight, width: div.offsetWidth, renderTo: id }, meta);
         var swarm = new Cla.Swarm(config);
-    });
+    },true);
 });
 
