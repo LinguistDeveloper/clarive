bb72d708cf6c1228d2ac0d95b27eb4fe00e8bd7a
rodrigo <rodrigo@clarive.com>
2015-09-16 12:44:41 +0200

    enh: skip migration flag

--- lib/Clarive/Role/CheckMigrations.pm
+++ lib/Clarive/Role/CheckMigrations.pm
@@ -1,8 +1,15 @@
 package Clarive::Role::CheckMigrations;
 use Mouse::Role;
 
+has skip_migration_check  => qw(is ro isa Bool default 0);
+
 sub check_migrations {
     my $self = shift;
+
+    if( $self->skip_migration_check ) {
+        warn "WARN: Migrations skipped on user request\n";
+        return;
+    }
 
     require Clarive::Cmd::migra;
     my $migra = Clarive::Cmd::migra->new( app => $self->app, env => $self->env, opts => {} );
