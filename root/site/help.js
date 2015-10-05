3f88039d9bec2779f531608c6e7e930b8d0f3c83
rodrigo <rodrigo@clarive.com>
2015-10-05 21:39:23 +0200

    fix: unique help window limitation

--- root/site/help.js
+++ root/site/help.js
@@ -1,7 +1,5 @@
 /****** Clarive Help context methods *******/
 Cla.help_show = function(params) {
-    if( Cla.help_win != undefined ) Cla.help_win.close();
-
     var treeRoot = new Ext.tree.AsyncTreeNode({ draggable: false, checked: false });
     var docs_tree = new Cla.Tree({
         region: 'west', width: 300,
@@ -16,7 +14,7 @@ Cla.help_show = function(params) {
 
     var goto_doc = function(path, opts){
         if( !opts ) opts={};
-        Cla.help_win.mask();
+        help_win.mask();
         Cla.ajax_json('/help/get_doc', { path: path }, function(res){
             var data = res.data;
             var html = function(){/*
@@ -33,6 +31,7 @@ Cla.help_show = function(params) {
             doc_reader.update(html);
             doc_reader.doLayout();
             doc_reader.is_loaded = true;
+            help_win.setTitle( data.title || path );
             $(doc_reader.el.dom).find('a').each(function(){
                 var link = $(this);
                 $(this).on('click', function(){
@@ -45,15 +44,15 @@ Cla.help_show = function(params) {
                     return false;
                 });
             });
-            Cla.help_win.unmask();
-            if( ! opts.from_hist ) Cla.help_win.history_push( path );
+            help_win.unmask();
+            if( ! opts.from_hist ) help_win.history_push( path );
         });
     };
     docs_tree.getLoader().on('beforeload', function(lo){
-        Cla.help_win.mask();
+        help_win.mask();
     });
     docs_tree.getLoader().on('load', function(lo){
-        Cla.help_win.unmask();
+        help_win.unmask();
         if( lo.baseParams.query ){
             var docs = [];
             docs_tree.root.cascade(function(nd){
@@ -124,6 +123,7 @@ Cla.help_show = function(params) {
         */}.tmpl({ chi: html_chi });
         doc_reader.update(html);
         doc_reader.is_loaded = true;
+        help_win.setTitle( _('Help') );
     };
     var search_box = new Baseliner.SearchSimple({ 
         width: 240,
@@ -149,40 +149,39 @@ Cla.help_show = function(params) {
 
     var btn_left = new Ext.Button({ icon: IC('arrow_left.gif'), disabled: true, handler: function(){
         check_btns();
-        if( Cla.help_win.history_curr <= 0 )  return;
-        var path = Cla.help_win.history[ --Cla.help_win.history_curr ];
+        if( help_win.history_curr <= 0 )  return;
+        var path = help_win.history[ --help_win.history_curr ];
         if( path ) goto_doc( path, { from_hist : true } ); 
         check_btns();
     }});
     var btn_right = new Ext.Button({ icon: IC('arrow_right.gif'), disabled: true, handler: function(){
         check_btns();
-        if( Cla.help_win.history_curr >= Cla.help_win.history.length ) return;
-        var path = Cla.help_win.history[ ++Cla.help_win.history_curr ];
+        if( help_win.history_curr >= help_win.history.length ) return;
+        var path = help_win.history[ ++help_win.history_curr ];
         if( path ) goto_doc( path, { from_hist : true } ); 
         check_btns();
     }});
     var check_btns = function(){
-        if( Cla.help_win.history_curr <= 0 )  {
+        if( help_win.history_curr <= 0 )  {
             btn_left.disable();
         } else {
             btn_left.enable();
         }
-        if( Cla.help_win.history_curr >= Cla.help_win.history.length-1 ) {
+        if( help_win.history_curr >= help_win.history.length-1 ) {
             btn_right.disable();
         } else {
             btn_right.enable();
         }
     }
 
-    Cla.help_win = new Cla.Window({
-        id: 'clarive-help-win',
+    var help_win = new Cla.Window({
         height: 750,
         width: 1000,
         //top: 20, left: 3,
         autoScroll: true,
         title: _('Help'),
         titleCollapse: true,
-        closeAction: 'destroy',
+        //closeAction: 'destroy',
         layout: 'border',
         history: [],
         history_curr: -1,
@@ -191,44 +190,25 @@ Cla.help_show = function(params) {
         ],
         items: [ docs_tree, doc_reader ]
     });
-    Cla.help_win.mask = function(){
-        Cla.help_win.body.mask( String.format('<img src="{0}" />', '/static/images/loading.gif' ) ).setHeight(9999);
+    help_win.mask = function(){
+        help_win.body.mask( String.format('<img src="{0}" />', '/static/images/loading.gif' ) ).setHeight(9999);
     }
-    Cla.help_win.unmask = function(){
-        Cla.help_win.body.unmask();
+    help_win.unmask = function(){
+        help_win.body.unmask();
     }
-    Cla.help_win.history_push = function(path){
-        if( Cla.help_win.history[Cla.help_win.history.length-1] != path ) {// avoid dups
-            Cla.help_win.history.push( path );
+    help_win.history_push = function(path){
+        if( help_win.history[help_win.history.length-1] != path ) {// avoid dups
+            help_win.history.push( path );
         }
-        Cla.help_win.history_curr = Cla.help_win.history.length-1;
+        help_win.history_curr = help_win.history.length-1;
         check_btns();
     }
-        //Cla.help_win.getEl().fadeOut('l', { duration: .5 });
-        //Cla.help_win.hide();
-    Cla.help_win.show();
+        //help_win.getEl().fadeOut('l', { duration: .5 });
+        //help_win.hide();
+    help_win.show();
     //treeRoot.expand();
 };
 
-Cla.help_show_orig = function(params) {
-    if( Cla.help_win != undefined ) Cla.help_win.close();
-    Cla.help_win = new Ext.Window({
-        id: 'baseliner-help-win',
-        height: 600,
-        top: 20, left: 3,
-        autoScroll:true,
-        width: (params.width!=undefined?params.width: 350),
-        width: (params.height!=undefined?params.height: 500),
-        maximizable: true,
-        title: _('Help') + ': ' + params.title,
-        titleCollapse: true,
-        closeAction: 'destroy',
-        html: params.html?params.html:params.text
-    });
-        //Cla.help_win.getEl().fadeOut('l', { duration: .5 });
-        //Cla.help_win.hide();
-    Cla.help_win.show();
-};
 Cla.help_on = function() {
     //Cla.help_button.setIcon('/static/images/icons/lightbulb.png');
     Cla.help_button.setIconClass('help-on');
--- root/site/tabfu.js
+++ root/site/tabfu.js
@@ -203,67 +203,6 @@ if( Prefs.routing ) {
         }
     };
     
-    /****** Baseliner Help context methods *******/
-    Baseliner.help_show = function(params) {
-        if( Baseliner.help_win != undefined ) Baseliner.help_win.close();
-        Baseliner.help_win = new Ext.Window({
-            id: 'baseliner-help-win',
-            layout: 'fit',
-            top: 20, left: 3,
-            width: (params.width!=undefined?params.width: 350),
-            width: (params.height!=undefined?params.height: 500),
-            maximizable: true,
-            title: _('Help') + ': ' + params.title,
-            titleCollapse: true,
-            closeAction: 'hide',
-            html: params.html?params.html:params.text
-        });
-            //Baseliner.help_win.getEl().fadeOut('l', { duration: .5 });
-            //Baseliner.help_win.hide();
-        Baseliner.help_win.show();
-    };
-    Baseliner.help_on = function() {
-        //Baseliner.help_button.setIcon('/static/images/icons/lightbulb.png');
-        Baseliner.help_button.setIconClass('help-on');
-    };
-    Baseliner.help_off = function() {
-        //Baseliner.help_button.setIcon('/static/images/icons/lightbulb_off.png');
-        Baseliner.help_button.setIconClass('help-off');
-    };
-    Baseliner.help_handler = function(params) {
-        if( params.key != undefined ) {  // load by help key
-            Baseliner.ajaxEval( '/help/load', params, function(res) {
-                Baseliner.help_show(res);
-            });
-        } else if( params.path != undefined ) {
-            var req = Ext.Ajax.request({
-                url: '/help/load',
-                params: params,
-                success: function(res) {
-                    var body = res.responseText;
-                    Baseliner.help_show({ title: params.title, html: body });
-                }
-            });
-        } else if( params.text != undefined ) {
-            Baseliner.help_show( params );
-        } else {
-            Ext.Msg.alert( _('Help'), _('Help not available for this item.') );
-        }
-    };
-    Baseliner.help_push = function(params) {
-        try {  // Ext 2.x does not have a find
-        var items = Baseliner.help_menu.find( 'text', params.title );
-        //alert( JSON.stringify( items ));
-        if( items!=undefined && items.length > 0 ) return;
-        } catch(e) { }
-        Baseliner.help_menu.addMenuItem({
-            text: params.title,
-            handler: function() { Baseliner.help_handler(params) },
-            icon: (params.icon!=undefined ? params.icon : '/static/images/icons/help.png')
-        });
-        Baseliner.help_on();
-    };
-
     Baseliner.doLoginForm = function(lf, params, cb ){
         var ff = lf.getForm();
         params = params==undefined ? {} : params;
