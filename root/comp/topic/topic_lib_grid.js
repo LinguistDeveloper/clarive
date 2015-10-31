632ba78a20dd56271c3d1b9646b8d07ad69d11c3
rodrigo <rodrigo@clarive.com>
2015-10-31 01:43:40 +0100

    enh: open topic grid from fieldlet

--- root/comp/topic/topic_lib_grid.js
+++ root/comp/topic/topic_lib_grid.js
@@ -35,6 +35,13 @@ Baseliner.open_topic_grid = function(dir,title,mid){
    Baseliner.add_tabcomp('/comp/topic/topic_grid.js',  _('#%1 %2', mid, shorten_title( title )), gridp ); 
 };
 
+Cla.open_grid_from_field = function(el,mid,category,field_title){
+    var el = $(el);
+    var topics = [];
+    $(el).find('.topic-row').each(function(){ topics.push($(this).attr('mid')); });
+    Baseliner.add_tabcomp('/comp/topic/topic_grid.js', _('%1: %2 #%3', field_title, category, mid), { clear_filter: 1, topic_list: topics, tab_icon: '/static/images/icons/topic.png' });
+};
+
 Baseliner.open_monitor_query = function(q){
     Baseliner.add_tabcomp('/job/monitor', null, { query: q });
 }
