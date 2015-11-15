dc5b936df7b5653d2b101aae8c51fa82c073127c
vti <viacheslav.t@gmail.com>
2015-11-15 11:14:22 +0100

    fix: cla prove runs prove with correct arguments

--- lib/Clarive/Cmd/prove.pm
+++ lib/Clarive/Cmd/prove.pm
@@ -1,85 +1,17 @@
 package Clarive::Cmd::prove;
 use Mouse;
-use Path::Class;
-use Time::HiRes qw( usleep ualarm gettimeofday tv_interval nanosleep stat );
-extends 'Clarive::Cmd';
-use v5.10;
+BEGIN { extends 'Clarive::Cmd' }
 
-our $CAPTION = 'run system tests and check';
-with 'Clarive::Role::Baseliner';
-
-has type          => qw(is rw isa Str default server);
-has test_url      => qw(is rw isa Str), default => sub {
-    my ($self)=@_;
-    return $self->app->config->{baseliner}{web_url};
-};
-has test_user     => qw(is rw isa Str default local/root);
-has test_password => qw(is rw isa Str default admin);
-has case  => qw(is rw isa Str default *);
+our $CAPTION = 'run prove with correct parameters';
 
 sub run {
     my ( $self, %opts ) = @_;
-    
-    require Time::HiRes;
-    require Term::ANSIColor;
-    require Clarive::Test;
-    
-    $Clarive::Test::base_url = $self->test_url;
-    $Clarive::Test::user = $self->test_user;
-    $Clarive::Test::password = $self->test_password;
-    
-    my $grc=0;
-    
-    $self->type('*') if exists $opts{case};
 
-    my @tc = glob join '/', $self->home, 't', $self->type, $self->case . '*';
-    for my $tc ( sort @tc ) {
-        my $pid;
-        my ($ext) = $tc =~ /\.(\w+)$/;
-        unless ( $pid = fork ) {
-            say "====> [start] $tc" ;
-            my $t0 = [gettimeofday]; 
-            my $rc;
-            if( $ext eq 't' ) { 
-                do $tc;  # this returns 1 always... or whatever, unusable
-                $rc = $@;  # catch errors here
-            } elsif( $ext eq 'har' ) {
-                require Path::Class;
-                my $ag = Clarive::Test->user_agent;
-                my $har = Path::Class::file( $tc ); 
-                my $body = scalar $har->slurp;
-                utf8::encode( $body );
-                $rc = $ag->har_run( $body  );
-            }
-            my $inter = sprintf( "%.04fs", tv_interval( $t0 ) );
-            say Term::ANSIColor::color('red'),"====> [error] $tc:\n" . $@, Term::ANSIColor::color('reset') if $@;
-            say "====> [end] $tc [$inter]" ;
-            exit !!$rc;
-        }
-        waitpid $pid,0;
-        my $rc = $? >> 8;
-        $grc += $rc;
-    }
-    exit $grc;
+    my @argv = @{ $opts{argv} };
+
+    push @argv, 't' unless @argv;
+
+    exec("prove -Ilib -It/lib -r @argv");
 }
-
-sub run_startup {
-    my ( $self ) = @_;
-    $self->setup_baseliner;
-    $SIG{__WARN__} = sub {};
-    $ENV{BASELINER_DEBUG}=0;
-    say "Starting system test...";
-    eval {
-        require Baseliner;
-        Baseliner->build_app();
-    };
-    if( $@ ) {
-        die "Clarive: error during system prove: $@\n";
-    } else {
-        say "Clarive: all systems ready.";
-    }
-    exit 0;
-}
-
 
 1;
