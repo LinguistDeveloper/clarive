8924332057c9d2bcd3286b5780b2fa35e517344d
rodrigo <rodrigo@clarive.com>
2015-12-11 13:08:02 +0100

    fix: broken topic grid wont load

--- root/comp/topic/topic_lib_grid.js
+++ root/comp/topic/topic_lib_grid.js
@@ -1636,9 +1636,12 @@ Cla.topic_grid = function(params){
     });
     
     grid_topics.on( 'render', function(){
-        var gt_view = grid_topics.getView() || return;
-        var dom = gt_view.el.dom || return;
-        var el = dom.childNodes[0].childNodes[1] || return;
+        var gt_view = grid_topics.getView();
+        if( !gt_view ) return;
+        var dom = gt_view.el.dom;
+        if( !dom ) return;
+        var el = dom.childNodes[0].childNodes[1];
+        if( !el ) return;
         var grid_topics_dt = new Baseliner.DropTarget(el, {
             comp: grid_topics,
             ddGroup: 'explorer_dd',
