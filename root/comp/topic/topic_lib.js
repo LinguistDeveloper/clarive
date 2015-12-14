cc7efec971a76c2989597ba88fdbe51efe06a8ab
rodrigo <rodrigo@clarive.com>
2015-12-14 12:10:02 +0100

    fix: life_cicle error in topic

--- root/comp/topic/topic_lib.js
+++ root/comp/topic/topic_lib.js
@@ -955,7 +955,7 @@ Baseliner.TopicMain = Ext.extend( Ext.Panel, {
             cls: 'x-btn-icon',
             enableToggle: false, 
             tooltip: _('Open life cicle'),
-            handler: function(){ self.show_life_cicle() },
+            handler: function(){ self.show_lifecycle() },
             //hidden: self.viewKanban==undefined?true:!self.viewKanban,
             //allowDepress: false, 
             toggleGroup: self.toggle_group
