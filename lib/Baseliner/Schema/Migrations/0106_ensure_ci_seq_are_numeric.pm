bff42650d5ddb8fbc2e256522985dc89a2a81b74
vti <vti@clarive.com>
2016-01-26 12:47:33 +0100

    remove orphaned master_doc entries

--- lib/Baseliner/Schema/Migrations/0106_ensure_ci_seq_are_numeric.pm
+++ lib/Baseliner/Schema/Migrations/0106_ensure_ci_seq_are_numeric.pm
@@ -16,13 +16,25 @@ sub upgrade {
     my @cis_with_mixed_mids = sort { ( $a->{mid} =~ m/(\d+)$/ )[0] <=> ( $b->{mid} =~ m/(\d+)$/ )[0] }
       mdb->master->find( { mid => { '$regex' => '^[^\d]+-\d{1,5}$' } } )->fields( { _id => 0, mid => 1 } )->all;
 
+    my %master_mids = ();
     foreach my $ci ( @cis_with_numeric_mids, @cis_with_mixed_mids_aligned, @cis_with_mixed_mids ) {
         my $_seq = 0 + mdb->seq('ci-seq');
 
         warn "Migration: Applying sequence to mid=$ci->{mid} ==> seq=$_seq\n";
 
-        mdb->master->update( { mid => "$ci->{mid}" }, { '$set' => { _seq => $_seq } } );
-        mdb->master_doc->update( { mid => "$ci->{mid}" }, { '$set' => { _seq => $_seq } } );
+        mdb->master->update( { mid => "$ci->{mid}" }, { '$set' => { _seq => 0 + $_seq } } );
+        mdb->master_doc->update( { mid => "$ci->{mid}" }, { '$set' => { _seq => 0 + $_seq } } );
+
+        $master_mids{$ci->{mid}}++;
+    }
+
+    my $master_doc_iter = mdb->master_doc->find()->fields({_id => 0, mid => 1});
+    while( my $ci = $master_doc_iter->next ) {
+        if (!exists $master_mids{$ci->{mid}}) {
+            warn "Migration: Removing master_doc=$ci->{mid} because no related entry in master was found\n";
+
+            mdb->master_doc->remove({mid => "$ci->{mid}"});
+        }
     }
 
     mdb->master->ensure_index( { _seq => 1 }, { unique => 1 } );
