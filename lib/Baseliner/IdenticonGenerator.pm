c2455037ef2f67ae6bc7fb3e88349da63150b397
victor <victor@clarive.com>
2016-01-27 15:48:13 +0100

    well-format code in the file IdenticonGenerator.pm

--- lib/Baseliner/IdenticonGenerator.pm
+++ lib/Baseliner/IdenticonGenerator.pm
@@ -1,38 +1,46 @@
 package Baseliner::IdenticonGenerator;
 use Moose;
-use Baseliner::Utils qw(_debug);
+
 use Try::Tiny;
+use Image::Identicon;
+use Baseliner::Utils qw(_debug);
 
 has default_icon => qw(is ro);
 
 sub identicon {
-    my ($self, $username)=@_;
-    my $user = ci->user->find({ username=>$username })->next;
-    my $generate = sub {
-            # generate png identicon from random
-            require Image::Identicon;
-            my $salt = '1234';
-            my $identicon = Image::Identicon->new({ salt=>$salt });
-            my $image = $identicon->render({ code=> int(rand( 2 ** 32)), size=>32 });
-            return $image->{image}->png;
-    };
-    if( ref $user ) {
+    my ( $self, $username ) = @_;
+
+    my $user = ci->user->find_one( { username => $username } );
+
+    if ($user) {
         _debug "Generating and saving avatar";
-        my $png = try { 
-            $generate->();
-        } catch {
+        my $png = try {
+            $self->generate();
+        }
+        catch {
             my $user_png = $self->default_icon;
             $user_png->slurp;
         };
-        my $user_instance = ci->new($user->{mid});
-        $user_instance->update(avatar => $png);
+        my $user_instance = ci->new( $user->{mid} );
+        $user_instance->update( avatar => $png );
+
         #$user->update(avatar => $png);
         return $png;
     }
     else {
         _debug "User not found, avatar generated anyway";
-        return $generate->();
+        return $self->generate();
     }
+}
+
+sub generate {
+    my $self = shift;
+
+    my $salt = '1234';
+    my $identicon = Image::Identicon->new( { salt => $salt } );
+    my $image
+        = $identicon->render( { code => int( rand( 2**32 ) ), size => 32 } );
+    return $image->{image}->png;
 }
 
 1;
