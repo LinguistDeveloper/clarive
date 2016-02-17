f8952313fb1fc81a03757e912869822f2dc36ecf
Ricardo <ricardo@clarive.com>
2016-02-18 00:37:10 +0100

    enh: add checkbox to configuration panel for branch force creation

--- root/forms/git_create_branch.js
+++ root/forms/git_create_branch.js
@@ -1,10 +1,18 @@
 (function(params){
     var data = params.data || {};
 
+    var force = new Baseliner.CBox({
+        fieldLabel: _('Move to new sha if branch exists?'), 
+        name: 'force',
+        checked: data.force, 
+        default_value: false
+    });
+
     return [
         { xtype:'textfield', fieldLabel: _('Repository mid'), name: 'repo', value: data.repo },
         { xtype:'textfield', fieldLabel: _('Sha or Ref'), name: 'sha', value: data.sha },
-        { xtype:'textfield', fieldLabel: _('Branch Name'), name: 'branch', value: data.branch }
+        { xtype:'textfield', fieldLabel: _('Branch Name'), name: 'branch', value: data.branch },
+        force
     ]
 })
 
