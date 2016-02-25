c5128838bdc91858c07f181d2a343615d233e5da
Ricardo <ricardo@clarive.com>
2016-02-25 15:19:18 +0100

    enh: added GIT word in service description

--- root/forms/git_create_branch.js
+++ root/forms/git_create_branch.js
@@ -9,7 +9,8 @@
     });
 
     return [
-        { xtype:'textfield', fieldLabel: _('Repository mid'), name: 'repo', value: data.repo },
+        Baseliner.ci_box({ name: 'repo', role:'Baseliner::Role::CI::Repository', fieldLabel:_('Repository'), with_vars: 1, value: data.repo, force_set_value: true }),
+        // { xtype:'textfield', fieldLabel: _('Repository mid'), name: 'repo', value: data.repo },
         { xtype:'textfield', fieldLabel: _('Sha or Ref'), name: 'sha', value: data.sha },
         { xtype:'textfield', fieldLabel: _('Branch Name'), name: 'branch', value: data.branch },
         force
