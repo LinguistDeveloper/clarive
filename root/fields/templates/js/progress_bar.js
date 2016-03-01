8e19551bcf2d83dedaee2bee55d5089723f35767
Beatriz Muñoz <beatriz@clarive.com>
2016-03-01 20:24:49 +0100

    fix: progress var size in edit mode

--- root/fields/templates/js/progress_bar.js
+++ root/fields/templates/js/progress_bar.js
@@ -12,11 +12,10 @@ params:
 (function(params){
 	var data = params.topic_data;
 	var meta = params.topic_meta;
-	
     return [
 		{ xtype:'sliderfield', fieldLabel: _(meta.name_field), name: meta.id_field,
 			value: data ? eval('data.' + meta.bd_field) : 0,
-			anchor: '40%', tipText: function(thumb){
+			anchor: '100%', tipText: function(thumb){
 					return String(thumb.value) + '%';
 			},
 			disabled: Baseliner.eval_boolean(meta.readonly),
