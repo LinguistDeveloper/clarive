17c2adab853187139314f15860abeaee5415d2a2
elena <elena@clarive.com>
2016-03-17 08:39:21 +0100

    fixed mask in logfile

--- root/comp/job_logfile.js
+++ root/comp/job_logfile.js
@@ -23,16 +23,20 @@
         var panel = new Ext.Panel({
             layout: 'fit',
             tbar: [
-                { xtype: 'button', icon: '/static/images/icons/html.gif', style: 'width: 30px', cls: 'x-btn-icon',
-                    text: _('H'), handler: function(){ Baseliner.open_pre_page( panel.title, logfile.getValue() ) } },
+                { xtype: 'button', icon: '/static/images/icons/html.gif', cls: 'x-btn-icon', handler: function(){ Baseliner.open_pre_page( panel.title, logfile.getValue() ) } },
                 { xtype: 'button', text: _('Reload'), handler: load_logfile, icon:'/static/images/icons/refresh.png', cls:'x-btn-text-icon' }
             ],
             tab_icon: '/static/images/icons/page.gif',
             items: [ logfile ]
         });
         panel.on('afterrender', function(){
-            panel.body.setStyle({ 'overflow': 'hidden' });
             load_logfile();
         });
+        panel.on("activate", function() {
+            Baseliner.showLoadingMask( panel.getEl());
+    });
+        panel.on('destroy', function(){
+            autorefresh.stop(task);
+    });
         return panel;
 })
