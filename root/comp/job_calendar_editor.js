9bc4420ed77dfc6fce66e19973f6340b06bd8c55
elena <elena@clarive.com>
2016-04-06 11:39:33 +0200

    cleanup(#32797):improved theme in job slots edit window-II

--- root/comp/job_calendar_editor.js
+++ root/comp/job_calendar_editor.js
@@ -25,102 +25,114 @@
         title: _('Calendar Info'),
         autoHeight: true,
         autoWidth: true,
-        defaults: { width: 300 },
-        items: [                
-            { layout:'column', anchor:'90%',  
-            items:[
-                { layout: 'form', columnWidth: 0.5, defaults:{ anchor:'90%' },
-                items: [
-                    {  xtype: 'textfield',
-                        fieldLabel: _loc('Name'),
-                        name: 'name',
-                        value: '<% $cal->{name} %>'
+        defaults: {
+            width: 300
+        },
+        items: [{
+            layout: 'column',
+            anchor: '90%',
+            items: [{
+                layout: 'form',
+                columnWidth: 0.5,
+                defaults: {
+                    anchor: '90%'
+                },
+                items: [{
+                    xtype: 'textfield',
+                    fieldLabel: _loc('Name'),
+                    name: 'name',
+                    value: '<% $cal->{name} %>'
+                }, {
+                    xtype: 'textfield',
+                    fieldLabel: _loc('Priority'),
+                    name: 'seq',
+                    value: '<% $cal->{seq} %>'
+                }, {
+                    xtype: 'textarea',
+                    fieldLabel: _('Description'),
+                    name: 'description',
+                    value: '<% $cal->{description} %>'
+                }]
+            }, {
+                layout: 'form',
+                columnWidth: 0.5,
+                items: [{
+                        xtype: 'combo',
+                        name: 'bl',
+                        hiddenName: 'bl',
+                        fieldLabel: _('Baseline'),
+                        mode: 'local',
+                        editable: false,
+                        forceSelection: true,
+                        triggerAction: 'all',
+                        store: bl_store,
+                        valueField: 'value',
+                        value: '<% $cal->{bl}  %>',
+                        displayField: 'name',
+                        allowBlank: false
                     },
-                    {  xtype: 'textfield',
-                        fieldLabel: _loc('Priority'),
-                        name: 'seq',
-                        value: '<% $cal->{seq} %>'
-                    },
-                    {  xtype: 'textarea',
-                        fieldLabel: _('Description'),
-                        name: 'description',
-                        value: '<% $cal->{description} %>'
-                    } 
-                ] },
-                { layout: 'form', columnWidth: 0.5, 
-                items: [
-                    {  xtype: 'combo', 
-                               name: 'bl', 
-                               hiddenName: 'bl',
-                               fieldLabel: _('Baseline'),
-                               mode: 'local', 
-                               editable: false,
-                               forceSelection: true,
-                               triggerAction: 'all',
-                               store: bl_store, 
-                               valueField: 'value',
-                               value: '<% $cal->{bl}  %>',
-                               displayField:'name', 
-                               allowBlank: false
-                    },
-                    Baseliner.ci_box({ 
-                        name:'ns', 
-                        role:['Infrastructure','Project'], 
-                        width: 400, 
-                        fieldLabel:_('Scope'), 
-                        value: cal_ns != '/' ? cal_ns : '/' , 
-                        emptyText: _('Global'), 
-                        force_set_value: cal_ns && cal_ns != '/' ? true: false  
+                    Baseliner.ci_box({
+                        name: 'ns',
+                        role: ['Infrastructure', 'Project'],
+                        width: 400,
+                        fieldLabel: _('Scope'),
+                        value: cal_ns != '/' ? cal_ns : '/',
+                        emptyText: _('Global'),
+                        force_set_value: cal_ns && cal_ns != '/' ? true : false
                     }),
                 ],
-                buttons: [                  
-                    /*{  text: _('Ayuda'),
-                        handler: function(){ 
-                            Ext.Msg.show({
-                               title:'Ayuda sobre la herencia',
-                               msg: calendar_type_help,
-                               buttons: Ext.Msg.OK,
-                               animEl: 'elId'
-                            });
-                        } 
-                    },*/
-                    {  icon: '/static/images/icons/save.png',
-                        //text: _loc('Update'),
-                        text: _loc('Save'),
-                        handler: function(){ 
-                            var ff = cal_form.getForm();
-                            ff.submit({
-                                params: { id_cal: id_cal },
-                                success: function(form, action) { 
-                                    Baseliner.message(_('Calendar'), action.result.msg);
-                                    if( !id_cal || id_cal == '' || id_cal == -1  ) {
-                                        id_cal = action.result.id_cal;
-                                        cal_slots.load({ url: '/job/calendar_slots', params: { panel: id, id_cal: id_cal, scripts: true  } });
-                                        cal_windows.show();
-                                    }
-                                },
-                                failure: function(form, action) { Baseliner.message(_('Failure'), action.result.msg); }
-                            });
-                        } 
-                    },
-                    {  icon: '/static/images/icons/delete_.png',
-                        text: _loc('Delete'),
-                        handler: function(){ 
-                            var ff = cal_form.getForm();
-                            ff.submit({
-                                params: { action: 'delete', id_cal: id_cal },
-                                success: function(form, action) { 
-                                    Baseliner.message(_('Calendar'), action.result.msg);
-                                    id_cal = '';
-                                    cal_windows.hide();
-                                },
-                                failure: function(form, action) { Baseliner.message(_('Failure'), action.result.msg); }
-                            });
-                        } 
-                    }                   
-               ]},
-            ]}
-        ]
+                buttons: [{
+                    icon: '/static/images/icons/save.png',
+                    text: _loc('Save'),
+                    handler: function() {
+                        var ff = cal_form.getForm();
+                        ff.submit({
+                            params: {
+                                id_cal: id_cal
+                            },
+                            success: function(form, action) {
+                                Baseliner.message(_('Calendar'), action.result.msg);
+                                if (!id_cal || id_cal == '' || id_cal == -1) {
+                                    id_cal = action.result.id_cal;
+                                    cal_slots.load({
+                                        url: '/job/calendar_slots',
+                                        params: {
+                                            panel: id,
+                                            id_cal: id_cal,
+                                            scripts: true
+                                        }
+                                    });
+                                    cal_windows.show();
+                                }
+                            },
+                            failure: function(form, action) {
+                                Baseliner.message(_('Failure'), action.result.msg);
+                            }
+                        });
+                    }
+                }, {
+                    icon: '/static/images/icons/delete_.png',
+                    text: _loc('Delete'),
+                    handler: function() {
+                        var ff = cal_form.getForm();
+                        ff.submit({
+                            params: {
+                                action: 'delete',
+                                id_cal: id_cal
+                            },
+                            success: function(form, action) {
+                                Baseliner.message(_('Calendar'), action.result.msg);
+                                id_cal = '';
+                                cal_windows.hide();
+                            },
+                            failure: function(form, action) {
+                                Baseliner.message(_('Failure'), action.result.msg);
+                            }
+                        });
+                    }
+                }]
+            }, ]
+        }]
     });
 
     var cal_slots = new Ext.Panel({    
