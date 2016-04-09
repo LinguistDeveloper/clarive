e87e2fd9a61ab2864479cbd5278598baff7ac07a
eugeniu <eugeniu@clarive.com>
2016-04-09 15:11:37 +0200

    control language sensitive

--- root/forms/topic_get_with_condition.js
+++ root/forms/topic_get_with_condition.js
@@ -6,17 +6,38 @@
     var ccategory = new Baseliner.CategoryBox({ name: 'categories', fieldLabel: _('Select topics in categories'), value: data.categories || ''  });
 
 
+     var store_values = new Ext.data.SimpleStore({
+        fields: ['assigned_to', 'name'],
+        data:[ 
+            [ '0', _('Any') ],
+            [ '1', _('Current') ],
+        ]
+    });
+
+
+    var value_combo = new Ext.form.ComboBox({
+        store: store_values,
+        displayField: 'name',
+        value: data.assigned_to,
+        valueField: 'assigned_to',
+        hiddenName: 'assigned_to',
+        name: 'assigned_to',
+        editable: false,
+        mode: 'local',
+        forceSelection: true,
+        triggerAction: 'all', 
+        fieldLabel: _('User assigned to topics'),
+        autoLoad: true,
+        anchor: '100%',
+    });
+
+
     return [
         ccategory,
         cstatus,
         { xtype : "checkbox", name : "not_in_status", checked: data.not_in_status=='on' ? true : false, boxLabel : _('Exclude selected statuses?') },
         // { xtype:'textfield', fieldLabel: _('User assigned to topics'), name: 'assigned_to', value: data.assigned_to },
-        new Baseliner.ComboSingle({ forceSelection: false, allowBlank: true, fieldLabel: _('User assigned to topics'), editable: true, name: 'assigned_to', value: data.assigned_to || '', data: [
-            [''],
-            [_('Current')],
-            [_('Any')]
-          ] 
-        }),
+        value_combo,
         { xtype:'textfield', anchor:'100%', fieldLabel: _('Advanced JSON/MongoDB condition for filter'), name: 'condition', value: data.condition }
     ];
 })
