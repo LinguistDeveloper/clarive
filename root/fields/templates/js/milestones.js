51e5ed95491863920af3dd2e6783036ef669cede
diego <diego@clarive.com>
2016-05-11 16:22:02 +0200

    validate dates in env_planner and milestones

--- root/fields/templates/js/milestones.js
+++ root/fields/templates/js/milestones.js
@@ -11,7 +11,7 @@ params:
     allowBlank: 'false'
     height: 300
     width: 100%
-    columns: 'Milestone[slotname];Planned End Date[plan_end_date],datefield;End Date[end_date],datefield' 
+    columns: 'Milestone[slotname];Planned End Date[plan_end_date],datefield;End Date[end_date],datefield'
     section: 'head'
     relation: system
     meta_type: 'calendar'
@@ -20,7 +20,7 @@ params:
 (function(params){
     var meta = params.topic_meta;
     var data = params.topic_data;
-    
+
     var value = data[ meta.bd_field ] || meta.default_value ;
     var records = data && data[ meta.bd_field ]? data[ meta.bd_field ] : '[]';
     var columns = meta.columns;
@@ -40,10 +40,10 @@ params:
             columns = arr_columns.join(';');
         };
     }
-    
+
     var allow = Baseliner.eval_boolean(meta.allowBlank);
     var readonly = Baseliner.eval_boolean(meta.readonly);
-    
+
     var editor = new Baseliner.GridEditor({
         fieldLabel: _(meta.name_field),
         cls: 'milestone_table',
@@ -51,7 +51,7 @@ params:
         name: meta.id_field,
         id_field: meta.id_field,
         bd_field: meta.bd_field,
-        records: records, 
+        records: records,
         columns: columns,
         font: meta.font,
         //anchor: meta.anchor || '100%',
@@ -64,7 +64,64 @@ params:
         enableDragDrop: !readonly,
         use_row_editor: !readonly
     });
+    if ( editor && editor.editor ) {
+        editor.editor.on('validateedit',function(ed, changes, r, rowIndex){
 
+            var row_final = Ext.apply({},changes);
+            var r_data = Ext.apply({},r.data);
+
+            if( row_final.start_date > row_final.end_date && row_final.plan_start_date > row_final.plan_end_date ) {
+                Cla.warning(_(meta.name_field),_('Planned start and start date are after ends dates'));
+                return false;
+            }else{
+                if( row_final.plan_start_date > row_final.plan_end_date ) {
+                    Cla.warning(_(meta.name_field),_('Planned start date is after planned end date'));
+                    return false;
+                }
+                if( row_final.start_date > row_final.end_date ) {
+                    Cla.warning(_(meta.name_field),_('Start date is after end date'));
+                    return false;
+                }
+            }
+
+            if( row_final.plan_start_date && !row_final.plan_end_date){
+                if(row_final.plan_start_date > r_data.plan_end_date && r_data.plan_end_date != ''){
+                    row_final.plan_start_date = r_data.plan_start_date;
+                    Cla.warning(_(meta.name_field),_('Planned start date is after planned end date'));
+                    return false;
+                }
+            }
+
+            if( !row_final.plan_start_date && row_final.plan_end_date){
+                if(r_data.plan_start_date > row_final.plan_end_date){
+                    row_final.plan_end_date = r_data.plan_end_date;
+                    Cla.warning(_(meta.name_field),_('Planned start date is after planned end date'));
+                    return false;
+                }
+
+            }
+
+            if( row_final.start_date && !row_final.end_date){
+                if(row_final.start_date > r_data.end_date && r_data.end_date != ''){
+                    row_final.start_date = r_data.start_date;
+                    Cla.warning(_(meta.name_field),_('Start date is after end date'));
+                    return false;
+                }
+            }
+
+            if( !row_final.start_date && row_final.end_date){
+                if(r_data.start_date > row_final.end_date){
+                    row_final.end_date = r_data.end_date;
+                    Cla.warning(_(meta.name_field),_('Start date is after end date'));
+                    return false;
+                }
+
+            }
+
+            var ret = true;
+            return ret;
+        });
+    }
     return [
         //Baseliner.field_label_top( meta.name_field, meta.hidden, allow, readonly ),
         //new Ext.Panel({
