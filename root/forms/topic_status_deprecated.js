4ae3811491b5f021d078edb877778bff594804e6
beatriz <beatriz@clarive.com>
2016-06-02 18:37:12 +0200

    fix: use correct form for service.topic.status

---
+++ root/forms/topic_status_deprecated.js
@@ -0,0 +1,9 @@
+(function(params){
+    var data = params.data || {};
+    return [
+        { xtype:'textfield', fieldLabel: _('Topics'), name: 'topics', value: data.topics },
+        { xtype:'textfield', fieldLabel: _('Old status'), name: 'old_status', value: data.old_status },
+        { xtype:'textfield', fieldLabel: _('New status'), name: 'new_status', value: data.new_status },
+        { xtype:'textfield', fieldLabel: _('User'), name: 'username', value: data.username }
+    ]
+})
