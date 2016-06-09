5e6ed721d09537c78eeaf8487306ccd763882f2b
vti <viacheslav.t@gmail.com>
2016-06-09 10:04:23 +0200

    correctly check old status when changing topic

--- root/forms/topic_status.js
+++ root/forms/topic_status.js
@@ -1,16 +1,32 @@
-(function(params){
+(function(params) {
     var data = params.data || {};
-    var oldstatus = new Baseliner.StatusBox({ name: 'old_status', fieldLabel: _('Old Statuses'), value: data.old_status || ''  });
-    var newstatus = new Baseliner.StatusBox({ name: 'new_status', fieldLabel: _('New Status'), singleMode:true, value: data.new_status || ''  });
 
-    return [
-        { xtype:'textfield', fieldLabel: _('Topics'), name: 'topics', value: data.topics },
-        oldstatus,
-        newstatus,
-        { xtype:'textfield', fieldLabel: _('User'), name: 'username', value: data.username }
+    var old_status = new Baseliner.StatusBox({
+        name: 'old_status',
+        fieldLabel: _('Old Statuses'),
+        value: data.old_status
+    });
+    var new_status = new Baseliner.StatusBox({
+        name: 'new_status',
+        fieldLabel: _('New Status'),
+        allowBlank: false,
+        singleMode: true,
+        value: data.new_status
+    });
+
+    return [{
+            xtype: 'textfield',
+            fieldLabel: _('Topics'),
+            name: 'topics',
+            allowBlank: false,
+            value: data.topics
+        },
+        old_status,
+        new_status, {
+            xtype: 'textfield',
+            fieldLabel: _('User'),
+            name: 'username',
+            value: data.username
+        }
     ]
 })
-
-
-
-
