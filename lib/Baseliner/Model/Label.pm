17ab5ab73ae11f9a343afb118135eafed093bf7c
ramon <ramon@clarive.com>
2016-08-10 09:57:55 +0200

    cleanup

--- lib/Baseliner/Model/Label.pm
+++ lib/Baseliner/Model/Label.pm
@@ -17,12 +17,6 @@ sub get_labels {
 
     @labels = mdb->label->find->all;
 
-    #user labels
-    # my $perm = Baseliner->model('Permissions');
-    # my @user_labels = mdb->label_user->find({username => $username})->all;
-    # if ($perm->user_has_action( username=> $username, action => 'action.labels.admin') || $perm->is_root( $username ) || $mode ne 'admin'  ){
-    # }
-
     return @labels;
 }
 
