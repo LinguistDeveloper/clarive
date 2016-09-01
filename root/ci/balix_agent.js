a6f81ab9d9300650879f118ffacad3d48ccbe40d
beatriz <beatriz@clarive.com>
2016-09-01 16:21:11 +0200

    enh: make server mandatory in role agent cis

--- root/ci/balix_agent.js
+++ root/ci/balix_agent.js
@@ -1,9 +1,9 @@
 (function(params){
     return [
-        Baseliner.ci_box({ name:'server', fieldLabel:_('Server'), allowBlank: true,
+        Baseliner.ci_box({ name:'server', fieldLabel:_('Server'), allowBlank: false,
                role:'Server', value: params.rec.server, force_set_value: true, singleMode: true }),
         { xtype: 'textfield', fieldLabel: _('User'), name:'user', anchor:'100%' },
-        { xtype: 'textfield', fieldLabel: _('Port'), name:'port', anchor:'100%', allowBlank: false },
+        { xtype: 'textfield', fieldLabel: _('Port'), name:'port', anchor:'100%', vtype: 'port', maxLength: 5, allowBlank: false },
         { xtype: 'textarea', fieldLabel: _('Key'), name:'key', height: 50, anchor:'100%', allowBlank: true },
         { xtype: 'textarea', fieldLabel: _('Chunk Size'), name:'chunk_size', height: 50, anchor:'100%', allowBlank: true }
     ]
--- root/ci/ssh_agent.js
+++ root/ci/ssh_agent.js
@@ -1,8 +1,9 @@
 (function(params){
     return [
-       Baseliner.ci_box({ name:'server', fieldLabel:_('Server'), role:'Server', value: params.rec.server, force_set_value:true}),
+       Baseliner.ci_box({ name:'server', fieldLabel:_('Server'), role:'Server',
+		value: params.rec.server, force_set_value:true, allowBlank: false }),
        { xtype:'textfield', fieldLabel: _('User'), name:'user', value: params.rec.user },
-       { xtype:'textfield', fieldLabel: _('Port'), name:'port_num', value: params.rec.port_num },
+       { xtype:'textfield', fieldLabel: _('Port'), name:'port_num', vtype: 'port', maxLength: 5, value: params.rec.port_num },
        { xtype:'textfield', fieldLabel: _('Timeout (s)'), name:'timeout', value: params.rec.timeout },
        { xtype:'textfield', anchor:'100%', fieldLabel: _('Private Key Filename'), name:'private_key', allowBlank: true }
     ]
