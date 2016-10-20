e879abfa4bc5f3dc3256b6aba6f5d974ff177cca
Victor Martinez <victor@clarive.com>
2016-10-20 15:26:55 +0200

    new: add box for default value in combo

--- root/fields/templates/config/combo.js
+++ root/fields/templates/config/combo.js
@@ -1,8 +1,17 @@
-(function(params){
-    var data = params.data || {};
-    var ret = Baseliner.generic_fields(data);
-    ret.push([ 
-    	{ xtype:'textfield',fieldLabel: _('Options'), name: 'options', value: data.options }
-    ]);
-    return ret;
+(function(params) {
+	Cla.help_push({ title:_('Combo'), path:'rules/palette/fieldlets/combo' });
+	var data = params.data || {};
+	var ret = Baseliner.generic_fields(data);
+	ret.push([{
+		xtype: 'textfield',
+		fieldLabel: _('Options'),
+		name: 'options',
+		value: data.options
+	}, {
+		xtype: 'textfield',
+		fieldLabel: _('Default Value'),
+		name: 'default_value',
+		value: data.default_value
+	}]);
+	return ret;
 })
