c889f6480c2f8bf98c7a519172a8ecad8ecfb4e5
raul <raul@clarive.com>
2016-12-19 11:34:20 +0100

    clean up

--- root/comp/topic/topic_lib_grid.js
+++ root/comp/topic/topic_lib_grid.js
@@ -1020,20 +1020,6 @@ Cla.topic_grid = function(params){
         };
         if( !value || value == undefined ) return '';
         return value;
-        //var value_to_date = new Date(value);
-        //return value_to_date.dateFormat('d/m/Y');
-        var date;
-        if (value.getMonth) {
-            date = value;
-        }else{
-            var dateStr= value;
-            if (dateStr == '' || dateStr == undefined) return '';
-            var a=dateStr.split(" ");
-            var d=a[0].split("-");
-            var t=a[1].split(":");
-            date = new Date(d[0],(d[1]-1),d[2],t[0],t[1],t[2]);
-        }
-        return date.dateFormat('d/m/Y');
     };
 
     var render_bool = function(value,metadata,rec,rowIndex,colIndex,store) {
