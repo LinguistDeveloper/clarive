72ef329244c6ffed3d7dfbf899637bbc4e20560f
vti <vti@clarive.com>
2017-01-20 15:22:15 +0100

    use IC for icons paths

--- root/site/common.js
+++ root/site/common.js
@@ -3301,8 +3301,8 @@ Cla.generateFieldletFilter = function(meta, mids) {
 
 Baseliner.render_checkbox = function(v){
     return v
-        ? '<img src="/static/images/icons/checkbox.svg">'
-        : '<img src="/static/images/icons/checkbox-empty.svg">';
+        ? '<img src="' + IC('checkbox') + '" />'
+        : '<img src="' + IC('checkbox-empty') + '" />';
 };
 Baseliner.render_avatar = function(v){
     return '<img width="16" src="/user/avatar/'+v+'/checkbox.svg">'
